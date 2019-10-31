876c61c8e3bdd661ee89caa1606f5853f269d8ec
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2019-10-31 11:10:24 +0100

    basics: helper function to wrap a Tensor2 with an Eigen::Map

---
+++ src/pitts_tensor2_eigen_adaptor.hpp
@@ -0,0 +1,44 @@
+/*! @file pitts_tensor2_eigen_adaptor.hpp
+* @brief Get an Eigen::Map for a simple PITTS::Tensor2
+* @author Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
+* @date 2019-10-31
+* @copyright Deutsches Zentrum fuer Luft- und Raumfahrt e. V. (DLR), German Aerospace Center
+*
+**/
+
+// include guard
+#ifndef PITTS_TENSOR2_EIGEN_ADAPTOR_HPP
+#define PITTS_TENSOR2_EIGEN_ADAPTOR_HPP
+
+// includes
+#include "pitts_tensor2.hpp"
+#include <Eigen/Dense>
+
+//! namespace for the library PITTS (parallel iterative tensor train solvers)
+namespace PITTS
+{
+  //! Get a const Eigen::Map for a Tensor2
+  //!
+  //! @tparam T  underlying data type (double, complex, ...)
+  //!
+  template<typename T>
+  auto ConstEigenMap(const Tensor2<T>& t2)
+  {
+    using Matrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
+    return Eigen::Map<const Matrix>(&t2(0,0), t2.r1(), t2.r2());
+  }
+
+  //! Get a mutable Eigen::Map for a Tensor2
+  //!
+  //! @tparam T  underlying data type (double, complex, ...)
+  //!
+  template<typename T>
+  auto EigenMap(Tensor2<T>& t2)
+  {
+    using Matrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
+    return Eigen::Map<Matrix>(&t2(0,0), t2.r1(), t2.r2());
+  }
+}
+
+
+#endif // PITTS_TENSOR2_EIGEN_ADAPTOR_HPP
---
+++ test/test_tensor2_eigen_adaptor.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+#include "pitts_tensor2_random.hpp"
+#include "pitts_tensor2_eigen_adaptor.hpp"
+#include <Eigen/Dense>
+
+TEST(PITTS_Tensor2_EigenAdaptor, simple_const)
+{
+  using Tensor2_double = PITTS::Tensor2<double>;
+
+  Tensor2_double t2(3,10);
+
+  randomize(t2);
+  const Tensor2_double t2_const = t2;
+  const auto t2Map = ConstEigenMap(t2_const);
+  ASSERT_EQ(t2.r1(), t2Map.rows());
+  ASSERT_EQ(t2.r2(), t2Map.cols());
+
+  for(int i = 0; i < t2.r1(); i++)
+    for(int j = 0; j < t2.r2(); j++)
+    {
+      EXPECT_EQ(t2(i,j), t2Map(i,j));
+    }
+}
+
+
+TEST(PITTS_Tensor2_EigenAdaptor, simple_modify)
+{
+  using Tensor2_double = PITTS::Tensor2<double>;
+
+  Tensor2_double t2(3,10);
+
+  randomize(t2);
+  auto t2Map = EigenMap(t2);
+  ASSERT_EQ(t2.r1(), t2Map.rows());
+  ASSERT_EQ(t2.r2(), t2Map.cols());
+
+  const Tensor2_double oldT2 = t2;
+  t2Map = Eigen::MatrixXd::Random(3,10);
+
+  for(int i = 0; i < t2.r1(); i++)
+    for(int j = 0; j < t2.r2(); j++)
+    {
+      EXPECT_EQ(t2(i,j), t2Map(i,j));
+      EXPECT_NE(oldT2(i,j), t2(i,j));
+    }
+}
