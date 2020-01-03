a49d5bb32675476b6151927e19d7c9de7c566ac9
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2020-01-03 11:53:11 +0100

    Also provide axpby for fixed-dimensions tensor train format

---
+++ src/pitts_fixed_tensortrain_axpby.hpp
@@ -0,0 +1,106 @@
+/*! @file pitts_fixed_tensortrain_axpby.hpp
+* @brief addition for simple tensor train format with fixed dimensions
+* @author Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
+* @date 2020-01-02
+* @copyright Deutsches Zentrum fuer Luft- und Raumfahrt e. V. (DLR), German Aerospace Center
+*
+**/
+
+// include guard
+#ifndef PITTS_FIXED_TENSORTRAIN_AXPBY_HPP
+#define PITTS_FIXED_TENSORTRAIN_AXPBY_HPP
+
+// includes
+#include <limits>
+#include "pitts_tensor2.hpp"
+#include "pitts_tensor2_qb_decomposition.hpp"
+#include "pitts_fixed_tensortrain.hpp"
+#include "pitts_fixed_tensor3_combine.hpp"
+#include "pitts_fixed_tensor3_split.hpp"
+
+//! namespace for the library PITTS (parallel iterative tensor train solvers)
+namespace PITTS
+{
+  //! Scale and add one tensor train to another
+  //!
+  //! Calculate gamma*y <- alpha*x + beta*y
+  //!
+  //! @tparam T  underlying data type (double, complex, ...)
+  //!
+  //! @param alpha          scalar value, coefficient of TTx
+  //! @param TTx            first tensor in tensor train format, must be leftNormalized
+  //! @param beta           scalar value, coefficient of TTy
+  //! @param TTy            second tensor in tensor train format, must be leftNormalized, overwritten with the result on output (still normalized!)
+  //!
+  template<typename T, int N>
+  void axpby(T alpha, const FixedTensorTrain<T, N>& TTx, T beta, FixedTensorTrain<T, N>& TTy)
+  {
+    // To add two tensor trains, for each sub-tensor, one obtains:
+    //
+    // a - a - ... - a - a
+    // |   |         |   |
+    //          +
+    // b - b - ... - b - b
+    // |   |         |   |
+    //          =
+    // axb - axb - ... - axb - axb
+    //  |     |           |     |
+    //
+    // with axb := (a 0;
+    //              0 b)
+    //
+    // Subsequent normalization...
+    //
+
+    // Auxiliary tensor of rank-3, for adjusting the size of the subtensors!
+    FixedTensor3<T, N> t3_tmp;
+
+    assert(TTx.nDims() == TTy.nDims());
+    const auto nDims = TTx.nDims();
+    for(int iDim = 0; iDim < nDims; iDim++)
+    {
+      const auto& subTx = TTx.subTensors()[iDim];
+      auto& subTy = TTy.editableSubTensors()[iDim];
+      const auto rx1 = subTx.r1();
+      const auto rx2 = subTx.r2();
+      const auto ry1 = subTy.r1();
+      const auto ry2 = subTy.r2();
+
+      const auto new_r1 = iDim == 0 ? 1 : rx1+ry1;
+      const auto new_r2 = iDim == nDims-1 ? 1 : rx2+ry2;
+      t3_tmp.resize(new_r1, new_r2);
+      t3_tmp.setConstant(T{});
+
+      // copy x to top left part...
+      T effAlpha = iDim == nDims-1 ? alpha : T(1);
+      for(int k = 0; k < rx2; k++)
+        for(int j = 0; j < N; j++)
+          for(int i = 0; i < rx1; i++)
+            t3_tmp(i,j,k) = effAlpha * subTx(i,j,k);
+
+      // copy y to bottom right part
+      T effBeta = iDim == nDims-1 ? beta : T(1);
+      const auto off1 = iDim == 0 ? 0 : rx1;
+      const auto off2 = iDim == nDims-1 ? 0 : rx2;
+      for(int k = 0; k < ry2; k++)
+        for(int j = 0; j < N; j++)
+          for(int i = 0; i < ry1; i++)
+            t3_tmp(i+off1,j,k+off2) += effBeta * subTy(i,j,k);
+
+      std::swap(subTy, t3_tmp);
+    }
+
+    // re-normalize with stupid combine/split...
+    for(int iDim = 0; iDim+1 < nDims; iDim++)
+    {
+      auto& subT1 = TTy.editableSubTensors()[iDim];
+      auto& subT2 = TTy.editableSubTensors()[iDim+1];
+      const auto subT12 = combine(subT1, subT2);
+      split(subT12, subT1, subT2);
+    }
+  }
+
+}
+
+
+#endif // PITTS_FIXED_TENSORTRAIN_AXPBY_HPP
---
+++ test/test_fixed_tensortrain_axpby.cpp
@@ -0,0 +1,191 @@
+#include <gtest/gtest.h>
+#include "pitts_fixed_tensortrain.hpp"
+#include "pitts_tensor2.hpp"
+#include "pitts_fixed_tensor3.hpp"
+#include "pitts_fixed_tensortrain_axpby.hpp"
+#include "pitts_fixed_tensortrain_dot.hpp"
+#include "pitts_fixed_tensortrain_random.hpp"
+
+namespace
+{
+  template<typename T>
+  auto pow2(T x)
+  {
+    return x*x;
+  }
+
+  static constexpr auto DIM = 5;
+
+  using FixedTensorTrain_double = PITTS::FixedTensorTrain<double, DIM>;
+  using Tensor2_double = PITTS::Tensor2<double>;
+  using FixedTensor3_double = PITTS::FixedTensor3<double, DIM>;
+  using mat = Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic>;
+  constexpr auto eps = 1.e-10;
+
+  double norm2(const FixedTensorTrain_double& TTv)
+  {
+    return std::sqrt(dot(TTv, TTv));
+  }
+
+  // helper function to contract a subtensor of the tensor train
+  Tensor2_double leftContract(const FixedTensor3_double& subT)
+  {
+    const auto r1 = subT.r1();
+    const auto r2 = subT.r2();
+    const auto n = subT.n();
+
+    Tensor2_double result(r2,r2);
+    for(int j = 0; j < r2; j++)
+      for(int i = 0; i < r2; i++)
+        result(i,j) = 0;
+    for(int k = 0; k < n; k++)
+      for(int j = 0; j < r2; j++)
+        for(int i = 0; i < r2; i++)
+          for(int i_ = 0; i_ < r1; i_++)
+            result(i,j) += subT(i_,k,i)*subT(i_,k,j);
+    return result;
+  }
+
+  // helper function to call axpby on a tensor train and do common checks
+  auto check_axpby(double alpha, const FixedTensorTrain_double& TTa, double beta, const FixedTensorTrain_double& TTb)
+  {
+    const double TTa_norm = norm2(TTa);
+    const double TTb_norm = norm2(TTb);
+
+    FixedTensorTrain_double TTresult = TTb;
+    axpby(alpha, TTa, beta, TTresult);
+    const double TTresult_norm = norm2(TTresult);
+
+    // check Law of cosines
+    EXPECT_NEAR(pow2(TTresult_norm), pow2(alpha*TTa_norm)+pow2(beta*TTb_norm) + 2*alpha*beta*dot(TTa,TTb), eps);
+
+    // check that the result is normalized
+    // except for the case alpha or beta == 0
+    if( alpha*beta != 0 )
+    {
+      // check orthogonality of subtensors
+      for(int iDim = 0; iDim < TTresult.nDims(); iDim++)
+      {
+        const auto& subT = TTresult.subTensors()[iDim];
+        mat ref;
+        if( iDim < TTresult.nDims()-1 )
+          ref = mat::Identity(subT.r2(), subT.r2());
+        else
+          ref = mat::Constant(1,1, pow2(TTresult_norm));
+        const mat orthogErr = ConstEigenMap(leftContract(subT)) - ref;
+        EXPECT_NEAR(0., orthogErr.norm(), eps);
+      }
+    }
+
+    // check that the result is correct
+    FixedTensorTrain_double testTT(TTresult.nDims());
+    if( TTresult.nDims() == 1 )
+    {
+      for(int i = 0; i < DIM; i++)
+      {
+        testTT.setUnit({i});
+        EXPECT_NEAR(alpha*dot(TTa,testTT)+beta*dot(TTb,testTT), dot(TTresult,testTT), eps);
+      }
+    }
+    else if( TTresult.nDims() == 2 )
+    {
+      for(int i = 0; i < DIM; i++)
+        for(int j = 0; j < DIM; j++)
+        {
+          testTT.setUnit({i,j});
+          EXPECT_NEAR(alpha * dot(TTa, testTT) + beta * dot(TTb, testTT), dot(TTresult, testTT), eps);
+        }
+    }
+    else
+    {
+      for(int i = 0; i < 10; i++)
+      {
+        randomize(testTT);
+        EXPECT_NEAR(alpha * dot(TTa, testTT) + beta * dot(TTb, testTT), dot(TTresult, testTT), eps);
+      }
+    }
+
+    return TTresult;
+  }
+}
+
+TEST(PITTS_FixedTensorTrain_axpby, corner_cases_rank1)
+{
+  FixedTensorTrain_double TTa(1), TTb(1);
+  randomize(TTa);
+  randomize(TTb);
+  check_axpby(0, TTa, 0, TTb);
+  check_axpby(0, TTa, 1, TTb);
+  check_axpby(0, TTa, 3, TTb);
+  check_axpby(1, TTa, 0, TTb);
+  check_axpby(3, TTa, 0, TTb);
+}
+
+TEST(PITTS_FixedTensorTrain_axpby, unit_vectors_same_direction)
+{
+  FixedTensorTrain_double TTa(1), TTb(1);
+  TTa.setUnit({1});
+  TTb.setUnit({1});
+
+  const auto TTc = check_axpby(4., TTa, 3., TTb);
+  EXPECT_NEAR(7., norm2(TTc), eps);
+}
+
+TEST(PITTS_FixedTensorTrain_axpby, unit_vectors_different_directions)
+{
+  FixedTensorTrain_double TTa(1), TTb(1);
+  TTa.setUnit({1});
+  TTb.setUnit({3});
+
+  const auto TTc = check_axpby(4., TTa, 3., TTb);
+  EXPECT_NEAR(5., norm2(TTc), eps);
+}
+
+TEST(PITTS_FixedTensorTrain_axpby, random_vectors)
+{
+  FixedTensorTrain_double TTa(1), TTb(1);
+  randomize(TTa);
+  randomize(TTb);
+
+  check_axpby(4., TTa, 3., TTb);
+}
+
+TEST(PITTS_FixedTensorTrain_axpby, rank2_unit_vectors_same_direction)
+{
+  FixedTensorTrain_double TTa(2), TTb(2);
+  TTa.setUnit({1,3});
+  TTb.setUnit({1,3});
+
+  const auto TTc = check_axpby(4., TTa, 3., TTb);
+  EXPECT_NEAR(7., norm2(TTc), eps);
+}
+
+TEST(PITTS_FixedTensorTrain_axpby, rank2_unit_vectors_different_directions)
+{
+  FixedTensorTrain_double TTa(2), TTb(2);
+  TTa.setUnit({1,1});
+  TTb.setUnit({3,3});
+
+  const auto TTc = check_axpby(4., TTa, 3., TTb);
+  EXPECT_NEAR(5., norm2(TTc), eps);
+}
+
+TEST(PITTS_FixedTensorTrain_axpby, rank2_random_vectors)
+{
+  FixedTensorTrain_double TTa(2,2), TTb(2,2);
+  randomize(TTa);
+  randomize(TTb);
+
+  check_axpby(4., TTa, 3., TTb);
+}
+
+TEST(PITTS_FixedTensorTrain_axpby, larger_random_tensor)
+{
+  FixedTensorTrain_double TTx(4), TTy(4);
+  TTx.setTTranks({2,3,4});
+  TTy.setTTranks({1,4,2});
+  randomize(TTx);
+  randomize(TTy);
+
+  check_axpby(2., TTx, -1.5, TTy);
+}
