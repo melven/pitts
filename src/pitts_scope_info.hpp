1f53dc89df1c256268c9d42cc7e8dd008b8b7f04
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2020-04-15 14:55:44 +0200

    scope_info: helper functionality for function name/arguments tracking

---
+++ src/pitts_scope_info.hpp
@@ -0,0 +1,77 @@
+/*! @file pitts_scope_info.hpp
+* @brief Helper class for storing/getting information about the current position in the code
+* @author Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
+* @date 2020-04-15
+* @copyright Deutsches Zentrum fuer Luft- und Raumfahrt e. V. (DLR), German Aerospace Center
+*
+**/
+
+// include guard
+#ifndef PITTS_SCOPE_INFO_HPP
+#define PITTS_SCOPE_INFO_HPP
+
+// includes
+#include <experimental/source_location>
+#include <array>
+
+
+//! namespace for the library PITTS (parallel iterative tensor train solvers)
+namespace PITTS
+{
+  //! namespace for helper functionality
+  namespace internal
+  {
+
+    //! Helper type to obtain and store the name of the current function / source file
+    struct ScopeInfo final : private std::experimental::source_location
+    {
+      //! constructor that obtains the location of the caller (when called without arguments!)
+      constexpr ScopeInfo(std::experimental::source_location here = std::experimental::source_location::current()) : std::experimental::source_location(here) {}
+
+      //! get the name of the enclosing function
+      using std::experimental::source_location::function_name;
+
+      //! get the name of the source file
+      using std::experimental::source_location::file_name;
+
+      //! get the line number in the source file
+      using std::experimental::source_location::line;
+    };
+
+
+    //! Helper type to store information on function/scope arguments
+    //!
+    //! mostly required for data dimensions, therefore we just support int argument values for now
+    //!
+    //! @tparam N number of arguments
+    //!
+    template<std::size_t N = 0>
+    struct ArgumentInfo final
+    {
+      //! argument names
+      std::array<const char*, N> names;
+
+      //! argument values
+      std::array<int, N> values;
+
+      //! helper function to make a readable string
+      std::string to_string() const
+      {
+        std::string result;
+        for(int i = 0; i < N; i++)
+        {
+          if( i > 0 )
+            result += ", ";
+          result += names[i];
+          result += ": ";
+          result += std::to_string(values[i]);
+        }
+        return result;
+      }
+    };
+
+  }
+}
+
+
+#endif // PITTS_SCOPE_INFO_HPP
---
+++ test/test_scope_info.cpp
@@ -0,0 +1,39 @@
+#include <gtest/gtest.h>
+#include "pitts_scope_info.hpp"
+
+namespace
+{
+  constexpr auto simpleFunction(int, int)
+  {
+    constexpr PITTS::internal::ScopeInfo scope;
+    return scope;
+  }
+
+  template<typename T>
+  constexpr auto templateFunction(T x)
+  {
+    constexpr PITTS::internal::ScopeInfo scope;
+    return scope;
+  }
+}
+
+TEST(PITTS_ScopeInfo, simpleFunction)
+{
+  constexpr auto scope = simpleFunction(7, 8);
+  EXPECT_EQ("simpleFunction", scope.function_name());
+}
+
+TEST(PITTS_ScopeInfo, templateFunction)
+{
+  constexpr auto scope = templateFunction(2.5);
+  EXPECT_EQ("templateFunction<double>", scope.function_name());
+}
+
+TEST(PITTS_ScopeInfo, ArgumentInfo)
+{
+  constexpr PITTS::internal::ArgumentInfo noArgs;
+  EXPECT_EQ("", noArgs.to_string());
+
+  constexpr PITTS::internal::ArgumentInfo<2> arg2Info{{"x", "y"}, {1, 8}};
+  EXPECT_EQ("x: 1, y: 8", arg2Info.to_string());
+}
