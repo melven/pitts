6419a5f4bb3222c73f533f61601d2f8831ccf10b
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2020-04-29 14:36:43 +0200

    kernel_info: add helper functionality for counting byte transfers

--- src/pitts_kernel_info.hpp
+++ src/pitts_kernel_info.hpp
@@ -21,37 +21,53 @@ namespace PITTS
   //! namespace for helper functionality
   namespace internal
   {
+    //! Helper data type for counting floating point operations of functions / kernels
     struct Flops final
     {
+      //! Does this kernel/operation *not* allow FMA (fused multiply add) operations
       bool noFMA;
-      double singlePrecision;
-      double doublePrecision;
 
+      //! number of single precision floating point operations
+      double singlePrecision;
+
+      //! number of double precision floating point operations
+      double doublePrecision;
     };
 
+    //! allow to multiply a Flops object with a scalar, e.g. the dimension of the data
     constexpr Flops operator*(double n, const Flops& f) noexcept
     {
       return {f.noFMA, n*f.singlePrecision, n*f.doublePrecision};
     }
 
+    //! allow to add two Flops objects
+    //!
+    //! For simplitiy, this currently sets the noFMA flag when any of the two operations does not support FMA.
+    //!
     constexpr Flops operator+(const Flops& a, const Flops& b) noexcept
     {
       return {a.noFMA || b.noFMA, a.singlePrecision + b.singlePrecision, a.doublePrecision + b.doublePrecision};
     }
 
+
+    //! Helper type for pre-defining Flops of common operations depending on the data type
     //!
-    //!
-    //! constexpr FlopsInfo flops = 20*Add<float> + 50*FMA<std::complex<double>>;
+    //! constexpr Flops flops = 20*Add<float>() + 50*FMA<std::complex<double>>();
     //! => flops.noFMAs = true;
     //!    flops.singlePrecision = 20;
-    //!    flops.doublePrecision = 200;
+    //!    flops.doublePrecision = 400; // 8 * 50
     //!
     //!
     template<bool B, int S, int D>
     struct BasicFlops
     {
+      //! Does this kernel/operation *not* allow FMA (fused multiply add) operations
       std::integral_constant<bool, B> noFMA;
+
+      //! number of single precision floating point operations
       std::integral_constant<int,  S> singlePrecision;
+
+      //! number of double precision floating point operations
       std::integral_constant<int,  D> doublePrecision;
 
       //! allow casting to Flops
@@ -62,11 +78,17 @@ namespace PITTS
     };
 
 
+    //! Flops class for adding two numbers, requires an appropriate template specialization
     template<typename> struct Add;
+
+    //! Flops class for multiplying two numbers, requires an appropriate template specialization
     template<typename> struct Mult;
+
+    //! Flops class for a fused-multiply-add operation of two numbers, requires an appropriate template specialization
     template<typename> struct FMA;
 
 
+    // template specializations for common numeric operations
     template<> struct Add<float>                  : public BasicFlops<false, 1, 0> {};
     template<> struct Add<double>                 : public BasicFlops<false, 0, 1> {};
     template<> struct Add<std::complex<float>>    : public BasicFlops<false, 2, 0> {};
@@ -81,17 +103,72 @@ namespace PITTS
     template<> struct FMA<std::complex<double>>   : public BasicFlops<true, 0, 8> {};
 
 
-/*
-RooflineKernelData = collections.namedtuple('RooflineKernelData', ['singlePrecisionFlops',
-                                                                   'doublePrecisionFlops',
-                                                                   'noFMAs',
-                                                                   'dataSize',
-                                                                   'updateBytes',
-                                                                   'loadBytes',
-                                                                   'storeBytes'])
-                                                                   */
+
+    //! helper class for counting data transfers
+    struct Bytes final
+    {
+      //! total size of the data, used to check wether the data fits into some cache...
+      double dataSize;
+
+      //! number of bytes that are read and written
+      double update;
+
+      //! number of bytes that are only read
+      double load;
+
+      //! number of bytes that are only written
+      double store;
+    };
+
+    //! allow to multiply a Bytes object with a scalar, e.g. the dimension of the data
+    constexpr Bytes operator*(double n, const Bytes& b) noexcept
+    {
+      return {n*b.dataSize, n*b.update, n*b.load, n*b.store};
+    }
+
+    //! allow to add two Bytes objects
+    constexpr Bytes operator+(const Bytes& a, const Bytes& b) noexcept
+    {
+      return {a.dataSize+b.dataSize, a.update+b.update, a.load+b.load, a.store+b.store};
+    }
 
 
+
+    //! Helper type for pre-defining Bytes of common operations depending on the data type
+    //!
+    //! constexpr Bytes bytes = 20*Load<float>() + 50*Update<std::complex<double>>();
+    //! => bytes.dataSize = 20*4 + 50*16;
+    //!    bytes.update = 50*16;
+    //!    bytes.load = 20*4;
+    //!    bytes.store = 0;
+    //!
+    template<int D, int U, int L, int S>
+    struct BasicBytes
+    {
+      //! total size of the data, used to check wether the data fits into some cache...
+      std::integral_constant<int, D> dataSize;
+
+      //! number of bytes that are read and written
+      std::integral_constant<int, U> update;
+
+      //! number of bytes that are only read
+      std::integral_constant<int, L> load;
+
+      //! number of bytes that are only written
+      std::integral_constant<int, S> store;
+
+      //! allow casting to Bytes
+      constexpr operator Bytes() const noexcept
+      {
+        return Bytes{double(dataSize), double(update), double(load), double(store)};
+      }
+    };
+
+
+    //! templates for common memory operations
+    template<typename T> struct Update   : public BasicBytes<sizeof(T), sizeof(T), 0, 0> {};
+    template<typename T> struct Load     : public BasicBytes<sizeof(T), 0, sizeof(T), 0> {};
+    template<typename T> struct Store    : public BasicBytes<sizeof(T), 0, 0, sizeof(T)> {};
   }
 }
 
--- test/test_kernel_info.cpp
+++ test/test_kernel_info.cpp
@@ -9,6 +9,14 @@ namespace
     EXPECT_NEAR(a.singlePrecision, b.singlePrecision, eps);
     EXPECT_NEAR(a.doublePrecision, b.doublePrecision, eps);
   }
+
+  void check_equal(const PITTS::internal::Bytes& a, const PITTS::internal::Bytes& b, const double eps = 1.e-8)
+  {
+    EXPECT_NEAR(a.dataSize, b.dataSize, eps);
+    EXPECT_NEAR(a.update, b.update, eps);
+    EXPECT_NEAR(a.load, b.load, eps);
+    EXPECT_NEAR(a.store, b.store, eps);
+  }
 }
 
 TEST(PITTS_KernelInfo, Flops_addition)
@@ -16,20 +24,26 @@ TEST(PITTS_KernelInfo, Flops_addition)
   constexpr double eps = 1.e-8;
   using namespace PITTS::internal;
 
-  Flops f = Flops{true, 1., 0.} + Flops{true, 2., 0.};
-  EXPECT_EQ(true, f.noFMA);
-  EXPECT_NEAR(3., f.singlePrecision, eps);
-  EXPECT_NEAR(0., f.doublePrecision, eps);
+  {
+    constexpr Flops f = Flops{true, 1., 0.} + Flops{true, 2., 0.};
+    EXPECT_EQ(true, f.noFMA);
+    EXPECT_NEAR(3., f.singlePrecision, eps);
+    EXPECT_NEAR(0., f.doublePrecision, eps);
+  }
 
-  f = Flops{true, 0., 3.} + Flops{false, 2., 2.};
-  EXPECT_EQ(true, f.noFMA);
-  EXPECT_NEAR(2., f.singlePrecision, eps);
-  EXPECT_NEAR(5., f.doublePrecision, eps);
+  {
+    constexpr Flops f = Flops{true, 0., 3.} + Flops{false, 2., 2.};
+    EXPECT_EQ(true, f.noFMA);
+    EXPECT_NEAR(2., f.singlePrecision, eps);
+    EXPECT_NEAR(5., f.doublePrecision, eps);
+  }
 
-  f = Flops{false, 0., 5.} + Flops{false, 0., 7.};
-  EXPECT_EQ(false, f.noFMA);
-  EXPECT_NEAR(0., f.singlePrecision, eps);
-  EXPECT_NEAR(12., f.doublePrecision, eps);
+  {
+    constexpr Flops f = Flops{false, 0., 5.} + Flops{false, 0., 7.};
+    EXPECT_EQ(false, f.noFMA);
+    EXPECT_NEAR(0., f.singlePrecision, eps);
+    EXPECT_NEAR(12., f.doublePrecision, eps);
+  }
 }
 
 TEST(PITTS_KernelInfo, Flops_multiplication)
@@ -37,20 +51,26 @@ TEST(PITTS_KernelInfo, Flops_multiplication)
   constexpr double eps = 1.e-8;
   using namespace PITTS::internal;
 
-  Flops f = 3*Flops{true, 1., 0.};
-  EXPECT_EQ(true, f.noFMA);
-  EXPECT_NEAR(3., f.singlePrecision, eps);
-  EXPECT_NEAR(0., f.doublePrecision, eps);
+  {
+    constexpr Flops f = 3*Flops{true, 1., 0.};
+    EXPECT_EQ(true, f.noFMA);
+    EXPECT_NEAR(3., f.singlePrecision, eps);
+    EXPECT_NEAR(0., f.doublePrecision, eps);
+  }
 
-  f = 0.5*Flops{true, 4., 10.};
-  EXPECT_EQ(true, f.noFMA);
-  EXPECT_NEAR(2., f.singlePrecision, eps);
-  EXPECT_NEAR(5., f.doublePrecision, eps);
+  {
+    constexpr Flops f = 0.5*Flops{true, 4., 10.};
+    EXPECT_EQ(true, f.noFMA);
+    EXPECT_NEAR(2., f.singlePrecision, eps);
+    EXPECT_NEAR(5., f.doublePrecision, eps);
+  }
 
-  f = 4*Flops{false, 0., 3.};
-  EXPECT_EQ(false, f.noFMA);
-  EXPECT_NEAR(0., f.singlePrecision, eps);
-  EXPECT_NEAR(12., f.doublePrecision, eps);
+  {
+    constexpr Flops f = 4*Flops{false, 0., 3.};
+    EXPECT_EQ(false, f.noFMA);
+    EXPECT_NEAR(0., f.singlePrecision, eps);
+    EXPECT_NEAR(12., f.doublePrecision, eps);
+  }
 }
 
 TEST(PITTS_KernelInfo, BasicFlops_predefs)
@@ -74,3 +94,46 @@ TEST(PITTS_KernelInfo, BasicFlops_predefs)
   check_equal(4*FMA<double>(), FMA<std::complex<double>>());
 }
 
+TEST(PITTS_KernelInfo, Bytes_addition)
+{
+  constexpr double eps = 1.e-8;
+  using namespace PITTS::internal;
+
+  constexpr Bytes b = Bytes{1., 10., 100., 1000.} + Bytes{3., 0.3, 0.03, 0.003};
+  EXPECT_NEAR(4., b.dataSize, eps);
+  EXPECT_NEAR(10.3, b.update, eps);
+  EXPECT_NEAR(100.03, b.load, eps);
+  EXPECT_NEAR(1000.003, b.store, eps);
+}
+
+TEST(PITTS_KernelInfo, Bytes_multiplication)
+{
+  constexpr double eps = 1.e-8;
+  using namespace PITTS::internal;
+
+  constexpr Bytes b = 0.5*Bytes{2., 3., 5., 7.};
+  EXPECT_NEAR(1., b.dataSize, eps);
+  EXPECT_NEAR(1.5, b.update, eps);
+  EXPECT_NEAR(2.5, b.load, eps);
+  EXPECT_NEAR(3.5, b.store, eps);
+}
+
+TEST(PITTS_KernelInfo, BasicBytes_predefs)
+{
+  using namespace PITTS::internal;
+
+  check_equal(Bytes{4., 4., 0., 0.}, Update<float>());
+  check_equal(Bytes{8., 8., 0., 0.}, Update<double>());
+  check_equal(Bytes{8., 8., 0., 0.}, Update<std::complex<float>>());
+  check_equal(Bytes{16., 16., 0., 0.}, Update<std::complex<double>>());
+
+  check_equal(Bytes{4., 0., 4., 0.}, Load<float>());
+  check_equal(Bytes{8., 0., 8., 0.}, Load<double>());
+  check_equal(Bytes{8., 0., 8., 0.}, Load<std::complex<float>>());
+  check_equal(Bytes{16., 0., 16., 0.}, Load<std::complex<double>>());
+
+  check_equal(Bytes{4., 0., 0., 4.}, Store<float>());
+  check_equal(Bytes{8., 0., 0., 8.}, Store<double>());
+  check_equal(Bytes{8., 0., 0., 8.}, Store<std::complex<float>>());
+  check_equal(Bytes{16., 0., 0., 16.}, Store<std::complex<double>>());
+}
