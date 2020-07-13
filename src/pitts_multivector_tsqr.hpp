9bbbb419d6513bc4c759dc97ae937889b560bf4e
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2020-07-13 19:13:37 +0200

    block_tsqr: helper function for 2 Householder rotations applied to 2 columns

--- src/pitts_multivector_tsqr.hpp
+++ src/pitts_multivector_tsqr.hpp
@@ -72,6 +72,8 @@ namespace PITTS
       //! This routine has the same effect as calling applyHouseholderRotation twice, first with vector w and then with vector v.
       //! Using this routine avoids to transfer required colums to/from the cache twice.
       //!
+      //! Exploits (I - v v^T) (I -w w^T) = I - v (v^T - v^T w w^T) - w w^T where v^T w can be calculated in advance.
+      //!
       //! @tparam T           underlying data type
       //!
       //! @param nChunks      number of rows divided by the Chunk size
@@ -85,7 +87,7 @@ namespace PITTS
       //! @param pdataResult  dense, column-major output array with dimension nChunks*#columns, the upper firstRow-1 chunks of rows are not touched
       //!
       template<typename T>
-      void apply2HouseholderRotations(int nChunks, int firstRow, int j, const Chunk<T>* w, const Chunk<T>* v, const Chunk<T> &vTw, const Chunk<T>* pdata, int lda, Chunk<T>* pdataResult)
+      void applyHouseholderRotation2(int nChunks, int firstRow, int j, const Chunk<T>* w, const Chunk<T>* v, const Chunk<T> &vTw, const Chunk<T>* pdata, int lda, Chunk<T>* pdataResult)
       {
         Chunk<T> wTx{};
         Chunk<T> vTx{};
@@ -100,7 +102,58 @@ namespace PITTS
         for(int i = firstRow; i < nChunks; i++)
         {
           fnmadd(wTx, w[i], pdata[i+lda*j], pdataResult[i+nChunks*j]);
-          fnmadd(vTx, v[i], pdataResult[i+nChunks*j], pdataResult[i+nChunks*j]);
+          fnmadd(vTx, v[i], pdataResult[i+nChunks*j]);
+        }
+      }
+
+
+      //! Apply two consecutive Householder rotations of the form (I - v v^T) (I - w w^T) with ||v|| = ||w|| = sqrt(2) to two columns
+      //!
+      //! Usually, a Householder rotation has the form (I - 2 v v^T), the factor 2 is included in v and w to improve the performance.
+      //!
+      //! This routine has the same effect as calling apply2HouseholderRotations twice, once for column col, once for column col+1
+      //! Using this routine avoids to transfer required Householder vectors from the cache twice.
+      //!
+      //! Exploits (I - v v^T) (I -w w^T) = I - v (v^T - v^T w w^T) - w w^T where v^T w can be calculated in advance.
+      //!
+      //! @tparam T           underlying data type
+      //!
+      //! @param nChunks      number of rows divided by the Chunk size
+      //! @param firstRow     index of the chunk that contains the current pivot element (0 <= firstRow < nChunks)
+      //! @param col          index of the first of the two consecutive columns.
+      //! @param w            first Householder vector with norm sqrt(2), the upper firstRow-1 chunks are ignored.
+      //! @param v            second Householder vector with norm sqrt(2), the upper firstRow-1 chunks are ignored.
+      //! @param vTw          scalar product of v and w; required as we apply both transformations at once
+      //! @param pdata        column-major input array with dimension nChunks*lda; can be identical to pdataResult for in-place calculation
+      //! @param lda          offset of columns in pdata
+      //! @param pdataResult  dense, column-major output array with dimension nChunks*#columns, the upper firstRow-1 chunks of rows are not touched
+      //!
+      template<typename T>
+      void applyHouseholderRotation2x2(int nChunks, int firstRow, int j, const Chunk<T>* w, const Chunk<T>* v, const Chunk<T> &vTw, const Chunk<T>* pdata, int lda, Chunk<T>* pdataResult)
+      {
+        Chunk<T> wTx{};
+        Chunk<T> vTx{};
+        Chunk<T> wTy{};
+        Chunk<T> vTy{};
+        for(int i = firstRow; i < nChunks; i++)
+        {
+          fmadd(w[i], pdata[i+lda*j], wTx);
+          fmadd(v[i], pdata[i+lda*j], vTx);
+          fmadd(w[i], pdata[i+lda*(j+1)], wTy);
+          fmadd(v[i], pdata[i+lda*(j+1)], vTy);
+        }
+        bcast_sum(wTx);
+        bcast_sum(vTx);
+        fnmadd(vTw, wTx, vTx);
+        bcast_sum(wTy);
+        bcast_sum(vTy);
+        fnmadd(vTw, wTy, vTy);
+        for(int i = firstRow; i < nChunks; i++)
+        {
+          fnmadd(wTx, w[i], pdata[i+lda*j], pdataResult[i+nChunks*j]);
+          fnmadd(vTx, v[i], pdataResult[i+nChunks*j]);
+          fnmadd(wTy, w[i], pdata[i+lda*(j+1)], pdataResult[i+nChunks*(j+1)]);
+          fnmadd(vTy, v[i], pdataResult[i+nChunks*(j+1)]);
         }
       }
 
