fb89dfbae2062efa6936151857b7645ce34d4e74
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2020-07-15 13:31:35 +0200

    chunk_ops: fix for unit test (incorrect ifdef)

--- test/test_chunk_ops_avx512.cpp
+++ test/test_chunk_ops_avx512.cpp
@@ -1,8 +1,8 @@
-#ifdef __AVX512__
+#ifdef __AVX512F__
 
 #include "pitts_chunk_ops_avx512.hpp"
 
 #define TEST_NAME PITTS_ChunkOps_AVX512
 #include "test_chunk_ops.hpp"
 
-#endif // __AVX512__
+#endif // __AVX512F__
