2ba12868c5bd651a3519614db6927bc601ff2925
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2020-07-15 12:32:02 +0200

    block_tsqr: use new driver method instead of prototype code in benchmark

--- src/tsqr_bench.cpp
+++ src/tsqr_bench.cpp
@@ -1,15 +1,13 @@
 #include <Eigen/Dense>
-#include "pitts_chunk.hpp"
-#include "pitts_chunk_ops.hpp"
+#include "pitts_multivector.hpp"
+#include "pitts_multivector_random.hpp"
 #include "pitts_multivector_tsqr.hpp"
+#include "pitts_tensor2.hpp"
+#include "pitts_tensor2_eigen_adaptor.hpp"
 #include <exception>
 #include <charconv>
 #include <iostream>
-#include <memory>
-#include <cmath>
-#include <limits>
 #include <omp.h>
-//#include <mkl.h>
 
 
 
@@ -46,84 +44,27 @@ int nThreads = 1;
   std::cout << "Iterations: " << nOuter << "\n";
   std::cout << "OpenMP Threads: " << nThreads << "\n";
 
+  PITTS::MultiVector<double> M(nPadded*nIter, m);
+  randomize(M);
 
-  std::unique_ptr<Chunk[]> pdataLarge{new Chunk[nChunks*nIter*m]};
-
-#pragma omp parallel
-{
-  std::unique_ptr<Chunk[]> pdataSmall{new Chunk[nChunks*m]};
-  std::unique_ptr<Chunk[]> plocalBuff{new Chunk[nChunks*m]};
-  Eigen::Map<mat> M(&pdataSmall[0][0], nPadded, m);
-  M = mat::Random(nPadded, m);
-  Eigen::Map<mat> Mlarge(&pdataLarge[0][0], nPadded*nIter, m);
-
-#pragma omp for schedule(static)
-  for(int iter = 0; iter < nIter; iter++)
-    Mlarge.block(iter*nPadded, 0, nPadded, m) = M;
-}
-
-  //Eigen::HouseholderQR<mat> qr(nPadded, m);
+  PITTS::Tensor2<double> R(m,m);
 
 double wtime = omp_get_wtime();
-
-  std::unique_ptr<Chunk[]> psharedBuff(new Chunk[mChunks*nThreads*m]);
-
-for(int i = 0; i < nOuter; i++)
-{
-
-#pragma omp parallel
-{
-  std::unique_ptr<Chunk[]> pdataSmall{new Chunk[nChunks*m]};
-  std::unique_ptr<Chunk[]> plocalBuff{new Chunk[nChunks*m]};
-
-  // fill with zero
-  for(int i = 0; i < nChunks; i++)
-    for(int j = 0; j < m; j++)
-      plocalBuff[i+nChunks*j] = Chunk{};
-
-  // index to the next free block in plocalBuff
-  int localBuffOffset = 0;
-
-#pragma omp for schedule(static)
-  for(int iter = 0; iter < nIter; iter++)
+  for(int iOuter = 0; iOuter < nOuter; iOuter++)
   {
-    PITTS::internal::HouseholderQR::transformBlock(nChunks, m, &pdataLarge[ nChunks*iter ], nChunks*nIter, &pdataSmall[0]);
-
-    // copy to local buffer
-    for(int j = 0; j < m; j++)
-      for(int i = 0; i < mChunks; i++)
-        plocalBuff[localBuffOffset + i + nChunks*j] = pdataSmall[i + nChunks*j];
-    localBuffOffset += mChunks;
-
-    if( localBuffOffset+mChunks > nChunks )
-    {
-      PITTS::internal::HouseholderQR::transformBlock(nChunks, m, &plocalBuff[0], nChunks, &plocalBuff[0]);
-      localBuffOffset = mChunks;
-    }
+    block_TSQR(M, R, reductionFactor);
   }
-// check if we need an additional reduction of plocalBuff
-if( localBuffOffset > mChunks )
-  PITTS::internal::HouseholderQR::transformBlock(nChunks, m, &plocalBuff[0], nChunks, &plocalBuff[0]);
-
-int offset = omp_get_thread_num()*mChunks;
-for(int j = 0; j < m; j++)
-  for(int i = 0; i < mChunks; i++)
-    psharedBuff[offset + i + nThreads*mChunks*j] = plocalBuff[i + nChunks*j];
-} // omp parallel
-
-}
 wtime = omp_get_wtime() - wtime;
 std::cout << "wtime: " << wtime << "\n";
 
   {
-    Eigen::Map<mat> M((double*)psharedBuff.get(), mChunks*Chunk::size*nThreads, m);
-    Eigen::BDCSVD<mat> svd(M);
+    Eigen::BDCSVD<mat> svd(ConstEigenMap(R));
     //std::cout << "Result:\n" << M << "\n";
     std::cout << "singular values (new):\n" << svd.singularValues().transpose() << "\n";
   }
 /*
   {
-    Eigen::Map<mat> Mlarge(&pdataLarge[0][0], nPadded*nIter, m);
+    Eigen::Map<mat> Mlarge(&M(0,0), nPadded*nIter, m);
     {
       Eigen::HouseholderQR<mat> qr(Mlarge.rows(), Mlarge.cols());
       wtime = omp_get_wtime();
