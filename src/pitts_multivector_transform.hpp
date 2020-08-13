8b56c927130a2faf41014a48c509bda8fdc89040
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2020-08-13 18:38:06 +0200

    multivector: add a faster fully aligned chunk-wise tsmm variant
    When both row lengths are a multiple of the chunk length,
    the tall-skiny matrix-matrix multiplication + reshaping can be performed
    faster.

--- src/pitts_multivector_transform.hpp
+++ src/pitts_multivector_transform.hpp
@@ -43,15 +43,91 @@ namespace PITTS
     if( reshape[0] * reshape[1] != X.rows()*M.r2() )
       throw std::invalid_argument("MultiVector::transform: invalid reshape dimensions!");
 
+    // check if we can do the fast aligned variant (depends on the reshape dimensions)
+    bool fast = X.rows() == Y.rows() ||
+                (X.rows() % Chunk<T>::size == 0 && Y.rows() % Chunk<T>::size == 0);
 
     // gather performance data
     const auto timer = PITTS::performance::createScopedTimer<MultiVector<T>>(
-        {{"Xrows", "Xcols", "Yrows", "Ycols"},{X.rows(),X.cols(),reshape[0],reshape[1]}}, // arguments
+        {{"Xrows", "Xcols", "Yrows", "Ycols", "fast"},{X.rows(),X.cols(),reshape[0],reshape[1],(long long)fast}}, // arguments
         {{(double(X.rows())*M.r1()*M.r2())*kernel_info::FMA<T>()}, // flops
          {(double(X.rows())*X.cols() + M.r1()*M.r2())*kernel_info::Load<T>() + (double(X.rows())*M.r2())*kernel_info::Store<T>()}} // data transfers
         );
 
     Y.resize(reshape[0], reshape[1]);
+    // special case without reshaping OR where both X and Y have #rows divisible by the chunk size
+    if( fast )
+    {
+#pragma omp parallel for schedule(static)
+      for(long long xChunk = 0; xChunk < X.rowChunks(); xChunk++)
+      {
+        long long yChunk = xChunk % Y.rowChunks();
+        long long yj = xChunk / Y.rowChunks();
+
+        long long mj = 0;
+        for(; mj+3 < M.r2(); mj+=4)
+        {
+          Chunk<T> tmp1{}, tmp2{}, tmp3{}, tmp4{};
+          for(long long k = 0; k < M.r1(); k++)
+          {
+            fmadd(M(k,mj+0), X.chunk(xChunk,k), tmp1);
+            fmadd(M(k,mj+1), X.chunk(xChunk,k), tmp2);
+            fmadd(M(k,mj+2), X.chunk(xChunk,k), tmp3);
+            fmadd(M(k,mj+3), X.chunk(xChunk,k), tmp4);
+          }
+
+          streaming_store(tmp1, Y.chunk(yChunk, yj));
+          yChunk += X.rowChunks();
+          while( yChunk > Y.rowChunks() )
+          {
+            yj++;
+            yChunk -= Y.rowChunks();
+          }
+
+          streaming_store(tmp2, Y.chunk(yChunk, yj));
+          yChunk += X.rowChunks();
+          while( yChunk > Y.rowChunks() )
+          {
+            yj++;
+            yChunk -= Y.rowChunks();
+          }
+
+          streaming_store(tmp3, Y.chunk(yChunk, yj));
+          yChunk += X.rowChunks();
+          while( yChunk > Y.rowChunks() )
+          {
+            yj++;
+            yChunk -= Y.rowChunks();
+          }
+
+          streaming_store(tmp4, Y.chunk(yChunk, yj));
+          yChunk += X.rowChunks();
+          while( yChunk > Y.rowChunks() )
+          {
+            yj++;
+            yChunk -= Y.rowChunks();
+          }
+        }
+
+        for(; mj < M.r2(); mj++)
+        {
+          Chunk<T> tmp{};
+          for(long long k = 0; k < M.r1(); k++)
+            fmadd(M(k,mj), X.chunk(xChunk,k), tmp);
+
+          streaming_store(tmp, Y.chunk(yChunk, yj));
+          yChunk += X.rowChunks();
+          while( yChunk > Y.rowChunks() )
+          {
+            yj++;
+            yChunk -= Y.rowChunks();
+          }
+        }
+      }
+      return;
+    }
+
+    // generic case where Y is reshaped
 #pragma omp parallel for schedule(static)
     for(long long yChunk = 0; yChunk < Y.rowChunks(); yChunk++)
     {
