f581850842abdea63b1d7ef8fcbef61e5e147e7d
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2020-08-17 12:41:59 +0200

    tensortrain: fromDense benchmark with larger outer dimensions
    To reduce memory transfers, the first/last dimension should already be
    larger than the maximal rank.

---
+++ src/tt_from_dense_tsqr_thickbounds_bench.cpp
@@ -0,0 +1,61 @@
+#include "pitts_common.hpp"
+#include "pitts_multivector.hpp"
+#include "pitts_multivector_random.hpp"
+#include "pitts_tensortrain.hpp"
+#include "pitts_tensortrain_from_dense_tsqr.hpp"
+#include <charconv>
+#include <vector>
+
+
+
+int main(int argc, char* argv[])
+{
+  //PITTS::initialize(&argc, &argv);
+
+  if( argc != 5 )
+    throw std::invalid_argument("Requires 4 arguments!");
+
+  std::size_t n = 0, d = 0, max_r = 0, nIter = 0;
+  std::from_chars(argv[1], argv[2], n);
+  std::from_chars(argv[2], argv[3], d);
+  std::from_chars(argv[3], argv[4], max_r);
+  std::from_chars(argv[4], argv[5], nIter);
+
+  std::size_t nTotal = 1;
+  std::vector<int> shape(d);
+  for(int i = 0; i < d; i++)
+  {
+    nTotal *= n;
+    shape[i] = n;
+  }
+
+  // compress shape, s.t. first and last dimensions are bigger than max_r
+  while( shape.size() > 2 && shape.front() < 1.7*max_r )
+  {
+    shape[1] *= shape[0];
+    shape.erase(shape.begin());
+  }
+  while( shape.size() > 2 && shape.back() < 1.7*max_r )
+  {
+    n = shape.size();
+    shape[n-2] *= shape[n-1];
+    shape.pop_back();
+  }
+
+  PITTS::MultiVector<double> data(nTotal/shape.back(), shape.back());
+  randomize(data);
+
+  PITTS::MultiVector<double> X(nTotal/shape.back(), shape.back());
+  PITTS::MultiVector<double> work(nTotal/shape.back(), shape.back());
+
+  for(int iter = 0; iter < nIter; iter++)
+  {
+    copy(data, X);
+    const auto TT = fromDense_TSQR(X, work, shape, 1.e-8, max_r);
+  }
+
+  PITTS::finalize();
+
+  return 0;
+}
+
