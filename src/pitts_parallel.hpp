9cb6cf2d65530280c36e2f31a00349d8cba93a21
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2020-08-19 22:05:39 +0200

    parallel: helper file for parallelization functionality

---
+++ src/pitts_parallel.hpp
@@ -0,0 +1,99 @@
+/*! @file pitts_parallel.hpp
+* @brief Helper functionality for parallelization (OpenMP, MPI)
+* @author Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
+* @date 2020-08-19
+* @copyright Deutsches Zentrum fuer Luft- und Raumfahrt e. V. (DLR), German Aerospace Center
+*
+**/
+
+// include guard
+#ifndef PITTS_PARALLEL_HPP
+#define PITTS_PARALLEL_HPP
+
+// includes
+#include <mpi.h>
+#include <unordered_map>
+#include <numeric>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <functional>
+#include <cereal/archives/binary.hpp>
+#include <cereal/types/string.hpp>
+#include <cereal/types/unordered_map.hpp>
+
+
+//! namespace for the library PITTS (parallel iterative tensor train solvers)
+namespace PITTS
+{
+  //! internal namespace for helper functionality concerning parallelization
+  namespace parallel
+  {
+    //! helper function to combine std::unordered_map from multiple MPI processes
+    //!
+    //! @tparam Key               std::unordered_map key type, must be serializable using cereal
+    //! @tparam Value             std::unordered_map value type, must be serializable using cereal
+    //! @tparam BinaryOperation   function signature to combine two values, defaults to + operator
+    //!
+    //! @param localMap           std::unordered_map on each process
+    //! @param op                 (optional) operator to combine values of keys that occur on multiple processes, only relevant on process 0
+    //! @param comm               (optional) MPI communicator, must be identical on all MPI processes
+    //!
+    template<typename Key, typename Value, class BinaryOperation = std::function<Value(Value,Value)>>
+    std::unordered_map<Key,Value> combineMaps(const std::unordered_map<Key,Value>& localMap, BinaryOperation op = std::plus<Value>(), MPI_Comm comm = MPI_COMM_WORLD)
+    {
+      // serialize data
+      std::ostringstream oss;
+      {
+        cereal::BinaryOutputArchive ar(oss);
+        ar( localMap );
+      }
+
+      // get number of processes
+      int nProcs = 1, iProc = 0;
+      if( MPI_Comm_size(comm, &nProcs) != 0 )
+        throw std::runtime_error("MPI error");
+      if( MPI_Comm_rank(comm, &iProc) != 0 )
+        throw std::runtime_error("MPI error");
+
+      // communicate raw data: all processes -> root
+      std::vector<int> sizes(nProcs, 0);
+      int localSize = oss.str().size();
+      if( MPI_Gather(&localSize, 1, MPI_INT, sizes.data(), 1, MPI_INT, 0, comm) != 0 )
+        throw std::runtime_error("MPI error");
+      std::vector<int> offsets;
+      std::exclusive_scan(sizes.begin(), sizes.end(), std::back_inserter(offsets), 0);
+      int totalSize = offsets.back() + sizes.back();
+      std::string raw(totalSize, '\0');
+      if( MPI_Gatherv(oss.str().data(), oss.str().size(), MPI_CHAR, raw.data(), sizes.data(), offsets.data(), MPI_CHAR, 0, comm) != 0 )
+        throw std::runtime_error("MPI error");
+
+      // put result from all processes into a global map
+      std::unordered_map<Key,Value> globalMap;
+      if( iProc == 0 )
+      {
+        for(int i = 0; i < nProcs; i++)
+        {
+          std::unordered_map<Key,Value> otherMap;
+          {
+            std::istringstream iss(raw.substr(offsets[i], sizes[i]));
+            cereal::BinaryInputArchive ar(iss);
+            ar( otherMap );
+          }
+          for(const auto& [key,val]: otherMap)
+          {
+            // try to insert the new key, merge key using op when it's already there
+            auto [iter,didInsert] = globalMap.insert({key,val});
+            if( !didInsert )
+              iter->second = op(iter->second, val);
+          }
+        }
+      }
+      return globalMap;
+    }
+
+  }
+}
+
+
+#endif // PITTS_PARALLEL_HPP
---
+++ test/test_parallel.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h>
+#include "pitts_parallel.hpp"
+
+
+TEST(PITTS_Parallel, combineMaps)
+{
+  using StringMap = std::unordered_map<std::string,std::string>;
+
+  StringMap localMap;
+  localMap["hello"] = "world";
+
+  const auto op = [](const std::string& s1, const std::string& s2){return s1 + " | " + s2;};
+
+  int nProcs = 1, iProc = 0;
+  ASSERT_EQ(0, MPI_Comm_size(MPI_COMM_WORLD, &nProcs));
+  ASSERT_EQ(0, MPI_Comm_rank(MPI_COMM_WORLD, &iProc));
+
+  StringMap globalMap = PITTS::parallel::combineMaps(localMap, op);
+
+  if( iProc == 0 )
+  {
+    ASSERT_EQ(1, globalMap.size());
+    std::string str_ref = "world";
+    for(int i = 1; i < nProcs; i++)
+      str_ref = str_ref + " | world";
+    ASSERT_EQ(str_ref, globalMap["hello"]);
+  }
+  else
+  {
+    ASSERT_EQ(0, globalMap.size());
+    ASSERT_EQ(0, 0);
+  }
+
+  localMap.clear();
+  localMap["proc"] = std::to_string(iProc);
+  localMap["only_local: "+std::to_string(iProc)] = "I'm here";
+
+  globalMap = PITTS::parallel::combineMaps(localMap);
+
+  if( iProc == 0 )
+  {
+    ASSERT_EQ(1+nProcs, globalMap.size());
+    std::string str_ref = "";
+    for(int i = 0; i < nProcs; i++)
+      str_ref = str_ref + std::to_string(i);
+    ASSERT_EQ(str_ref, globalMap["proc"]);
+    for(int i = 0; i < nProcs; i++)
+    {
+      std::string key = "only_local: " + std::to_string(i);
+      ASSERT_EQ("I'm here", globalMap[key]);
+    }
+  }
+  else
+  {
+    // dummy checks as all checks are global
+    ASSERT_EQ(0, globalMap.size());
+    ASSERT_EQ(0, 0);
+    for(int i = 0; i < nProcs; i++)
+    {
+      ASSERT_EQ(0, 0);
+    }
+  }
+}
