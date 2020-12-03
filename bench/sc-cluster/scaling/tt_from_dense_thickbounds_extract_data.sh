b870dcbf49afb4cd2eb696b5f042919949eaf891
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2020-12-03 17:18:14 +0100

    bench: updated runs for 1 and 2 sockets

---
+++ bench/sc-cluster/scaling/tt_from_dense_thickbounds_extract_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for f in *N1_n1*.out *N1_n2*.out *N1_n4*.out *N2_n8*.out; do
+  fgrep fromDense $f /dev/null | sed 's/tt_from_dense_thickbounds_d/ /' | sed 's/_N/  /' | sed 's/_n/  /' | sed 's/.sh-.*fromDense<double>/ /'
+  echo ""
+done
