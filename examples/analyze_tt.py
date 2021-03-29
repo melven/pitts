f27ba47649503d0508375f6fe10c0b419ef566db
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2021-03-29 17:30:42 +0200

    ATEX example: trying to improve data->TT-SVD

--- examples/analyze_tt.py
+++ examples/analyze_tt.py
@@ -19,70 +19,23 @@ def dict_to_tensortrain(tt_dict):
     return Xtt
 
 
-def multi_index(idx, sample_dims):
-    ii = list()
-    for d in reversed(sample_dims):
-        ii = [idx % d] + ii
-        idx = idx // d
-    return ii
-
-
-def eval_sample(Xtt, idx, sample_dims, feature_dims):
-    ii = multi_index(idx//batchSize, sample_dims)
-    tmp = np.ones(1, dtype=np.float32)
-    d_samples = len(ii)
-    d_features = len(Xtt.dimensions()) - d_samples
-    print('Xtt', Xtt)
-    print('sample_dims', sample_dims)
-    print('ii', ii)
-    for i in range(d_samples):
-        tmp = np.tensordot(tmp, Xtt.getSubTensor(i)[:,ii[i],:], ((0,),(0,)))
-
-    sample_tt = pitts_py.TensorTrain_float(Xtt.dimensions()[d_samples:])
-    sample_tt.setTTranks(Xtt.getTTranks()[d_samples:]) 
-    tmp = np.tensordot(tmp, Xtt.getSubTensor(d_samples)[:,:,:], ((0,),(0,)))
-    tmp = tmp.reshape((1, tmp.shape[0], tmp.shape[1]))
-    sample_tt.setSubTensor(0, tmp)
-    for i in range(1,d_features):
-        j = d_samples + i
-        sample_tt.setSubTensor(i, Xtt.getSubTensor(j))
-
-    return sample_tt
-
-
 if __name__ == '__main__':
 
     pitts_py.initialize(True)
 
-    batchSize=1
-    nProcs = 8
-    d_samples_global = 15
-    log2_nProcs = round(np.log2(nProcs))
-    d_samples_local = d_samples_global - log2_nProcs
     feature_dims = [192,1024,3]
 
-    iLastFile = None
-    for iFrame in range(32000, 32011):
+    iFile = 10
+    tt_dict = np.load('Xtt%d.npz'%iFile, allow_pickle=True)
+    Xtt = dict_to_tensortrain(tt_dict)
+    print(Xtt)
 
-        iFile = iFrame // (2**d_samples_local*batchSize)
-        ilocal = iFrame - iFile*2**d_samples_local*batchSize
+    X = pitts_py.toDense(Xtt)
+    X = X.reshape([X.shape[0],] + feature_dims, order='F')
 
-        if iLastFile != iFile:
-            tt_dict = np.load('Xtt%d.npz'%iFile, allow_pickle=True)
-            Xtt = dict_to_tensortrain(tt_dict)
-            iLastFile = iFile
-
-        sample_dims = [2,]*d_samples_local
-
-        print('iFile', iFile)
-        print('ilocal//batchSize', ilocal//batchSize)
-        sample_tt = eval_sample(Xtt, ilocal//batchSize, sample_dims, feature_dims+[batchSize,])
-        sample = pitts_py.toDense(sample_tt)
-        print('sample.shape', sample.shape)
-        sample = sample[...,ilocal%batchSize]
-        print('sample.shape', sample.shape)
-        sample = sample[...,ilocal%batchSize]
-        sample = sample.reshape(feature_dims, order='F')
+    for i in range(X.shape[0]):
+        iFrame = i + 30000 + X.shape[0]*iFile
+        sample = X[i,:]
         print('min', sample.min(), 'max', sample.max())
         sample = np.maximum(sample, 0)
         sample = np.minimum(sample, 1)
@@ -98,37 +51,6 @@ if __name__ == '__main__':
         img[:,:,1] = diff[:,:,1]
         img[:,:,2] = diff[:,:,0]
         plt.imsave('diff%d.png' % iFrame, img)
-
-    #n_samples = 100
-    #X = np.zeros((n_samples, *feature_dims), dtype=np.float32)
-    #for i in range(n_samples):
-    #    img = cv2.imread('/scratch/zoel_ml/ATEK_COPY/combustion050{:03d}.jpg'.format(i))
-    #    X[i,...] = img / 255
-    #dims = [n_samples,] + [2,]*6 + [3,] + [2,]*6 + [2*2*2*2*3,]
-    #Xm = pitts_py.MultiVector_float(np.prod(dims[:-1]), dims[-1])
-    #work = pitts_py.MultiVector_float(np.prod(dims[:-1]), dims[-1])
-    #Xm_view = np.array(Xm, copy=False)
-    #Xm_view[...] = X.reshape(Xm_view.shape, order='F')
-    #Xtt = pitts_py.fromDense(Xm, work, dims, rankTolerance=0.001, maxRank=500, mpiGlobal=True)
-    #print(Xtt)
-    #Y = pitts_py.toDense(Xtt)
-    #print(Y.shape)
-    #img0 = Y[0,...].reshape(feature_dims, order='F')
-    #img0 = np.maximum(img0, 0)
-    #img0 = np.minimum(img0, 1)
-    #img0rgb = np.zeros(feature_dims, dtype=np.float32)
-    #img0rgb[:,:,0] = img0[:,:,2]
-    #img0rgb[:,:,1] = img0[:,:,1]
-    #img0rgb[:,:,2] = img0[:,:,0]
-    #plt.imsave('sample050000.png', img0rgb)
-    #img1 = Y[1,...].reshape(feature_dims, order='F')
-    #img1 = np.maximum(img1, 0)
-    #img1 = np.minimum(img1, 1)
-    #img1rgb = np.zeros(feature_dims, dtype=np.float32)
-    #img1rgb[:,:,0] = img1[:,:,2]
-    #img1rgb[:,:,1] = img1[:,:,1]
-    #img1rgb[:,:,2] = img1[:,:,0]
-    #plt.imsave('sample050001.png', img1rgb)
 
     pitts_py.finalize(True)
 
--- examples/big_data_tt.py
+++ examples/big_data_tt.py
@@ -13,16 +13,29 @@ import pitts_py
 import sklearn.cluster
 
 
+def getCurrentMemoryUsage():
+    """Memory usage in GB"""
+    # from stackoverflow: https://stackoverflow.com/questions/938733/total-memory-used-by-python-process
+    with open('/proc/self/status') as f:
+        memusage = f.read().split('VmRSS:')[1].split('\n')[0][:-3]
+    return int(memusage.strip()) / 10**6
+
+
 def timer(func):
     """measure runtime of the decorated function"""
     @functools.wraps(func)
     def wrapper_fun(*args, **kwargs):
+        mem_before = getCurrentMemoryUsage()
         wtime = timeit.default_timer()
         value = func(*args, **kwargs)
         wtime = timeit.default_timer() - wtime
-        print(func.__name__, "wtime:", wtime)
+        mem_after = getCurrentMemoryUsage()
+        from mpi4py import MPI
+        if MPI.COMM_WORLD.Get_rank() == 0:
+            print(func.__name__, "wtime:", wtime, "memory useage (GB) before/after:", mem_before, mem_after)
         return value
     return wrapper_fun
+
 
 def distribute(nElems, mpiComm):
     iProc = mpiComm.Get_rank()
@@ -52,7 +65,7 @@ def read_data(file_template, n_samples, n_features, padWithZero=False):
     X = np.zeros((nlocal_samples, *n_features), dtype=np.float)
 
     for i in range(nlocal_samples):
-        file_name = file_template.format(first+i+1)
+        file_name = file_template.format(30000+first+i+1)
         img = cv2.imread(file_name)
         if img is None and padWithZero:
             X[i,...] = 0
@@ -69,11 +82,11 @@ def copy_to_multivector(X, dims, batchSize=1):
 
     n_total = np.prod(dims)
     Xm = pitts_py.MultiVector_float(n_total//dims[-1], dims[-1])
-    work = pitts_py.MultiVector_float()
     Xm_view = np.array(Xm, copy=False)
     # move dimension of batch samples to the right
-    X = X.reshape((X.shape[0]//batchSize, batchSize, *X.shape[1:]), order='C')
-    X = np.moveaxis(X, 1, -1)
+    assert(batchSize == 1)
+    #X = X.reshape((X.shape[0]//batchSize, batchSize, *X.shape[1:]), order='C')
+    #X = np.moveaxis(X, 1, -1)
     Xm_view[...] = X.reshape(Xm_view.shape, order='F')
 
     return Xm
@@ -100,29 +113,38 @@ if __name__ == '__main__':
 
     pitts_py.initialize(True)
     from mpi4py import MPI
+    nProcs = MPI.COMM_WORLD.Get_size()
+    iProc = MPI.COMM_WORLD.Get_rank()
+
+    # pad the number of samples so we get a multiple of 2^d*nProcs
+    n_samples = (116487 - 30000) // 4
+    n_features = (192,1024,3)
+    d = 0
+    while 2**d*nProcs < n_samples:
+        d = d+1
+    sample_dims = [nProcs,] + [2**d,]
+    feature_dims = [2,]*6 + [3,] + [2,]*8 + [2*2*3,]
+    assert(np.prod(sample_dims) < n_samples*2)
+    assert(np.prod(sample_dims) >= n_samples)
+    assert(np.prod(feature_dims) == np.prod(n_features))
+    n_samples_padded = np.prod(sample_dims)
 
     X = read_data(file_template='/scratch/zoel_ml/ATEK_COPY/combustion{:06d}.jpg',
-                  n_samples=2**15,
+                  n_samples=n_samples_padded,
                   n_features=(192,1024,3),
                   padWithZero=True)
 
-    # check if nProcs is power of 2 (otherwise unsupported
-    nProcs = MPI.COMM_WORLD.Get_size()
-    iProc = MPI.COMM_WORLD.Get_rank()
-    if nProcs & (nProcs-1) != 0:
-        raise 'nProcs must be a power of 2'
-    log2_nProcs = round(np.log2(nProcs))
-
-    dims=[2,]*(11-log2_nProcs) + [2,]*6 + [3,] + [2,]*10 + [3,] + [16,]
-    Xm = copy_to_multivector(X, dims, batchSize=1)
+    local_dims = sample_dims[1:] + feature_dims
+    Xm = copy_to_multivector(X, local_dims, batchSize=1)
     del X
 
     work = pitts_py.MultiVector_float()
     # just for comparison
     pitts_py.copy(Xm, work)
     MPI.COMM_WORLD.Barrier()
-    Xtt = calculate_TT_SVD(Xm, work, dims)
-    print(Xtt)
+    Xtt = calculate_TT_SVD(Xm, work, local_dims)
+    if MPI.COMM_WORLD.Get_rank() == 0:
+        print(Xtt)
 
     xtt_dict = tensortrain_to_dict(Xtt)
     np.savez(file='Xtt%d.npz'%iProc, **xtt_dict)
