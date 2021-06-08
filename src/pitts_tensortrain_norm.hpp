b212597b8af26be699ec0c3257fd1dcaec641f45
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2021-06-08 19:33:31 +0200

    tensortrain: try to improve performance of norm2 function

--- src/pitts_tensortrain_norm.hpp
+++ src/pitts_tensortrain_norm.hpp
@@ -22,6 +22,84 @@
 //! namespace for the library PITTS (parallel iterative tensor train solvers)
 namespace PITTS
 {
+  //! namespace for helper functionality
+  namespace internal
+  {
+    //! contract Tensor3 and Tensor2 along last dimensions: A(:,:,*) * B(:,*)
+    template<typename T>
+    void norm2_contract1(const Tensor3<T>& A, const Tensor2<T>& B, Tensor3<T>& C)
+    {
+      const auto r1 = A.r1();
+      const auto n = A.n();
+      const auto nChunks = A.nChunks();
+      const auto r2 = A.r2(); // == B.r1() == B.r2()
+      C.resize(r1, n, r2);
+
+      const auto timer = PITTS::performance::createScopedTimer<TensorTrain<T>>(
+        {{"r1", "nChunks", "r2"},{r1, nChunks, r2}}, // arguments
+        {{r1*nChunks*r2*r2*Chunk<T>::size*kernel_info::FMA<T>()}, // flops
+         {(r1*nChunks*r2+r2*r2)*kernel_info::Load<Chunk<T>>() + (r1*nChunks*r2)*kernel_info::Store<Chunk<T>>()}} // data transfers
+        );
+
+#pragma omp parallel for schedule(static)
+      for(int jChunk = 0; jChunk < nChunks; jChunk++)
+      {
+        for(int i = 0; i < r1; i++)
+        {
+          Chunk<T> tmp[r2]{};
+          for(int l = 0; l < r2; l++)
+            for(int k = 0; k < r2; k++)
+              fmadd(B(k,l), A.chunk(i,jChunk,l), tmp[k]);
+          for(int k = 0; k < r2; k++)
+            C.chunk(i,jChunk,k) = tmp[k];
+        }
+      }
+    }
+
+    //! contract Tensor3 and Tensor3 along the last two dimensions: A(:,*,*) * B(:,*,*)
+    //!
+    //! exploits the symmetry of the result in the norm calculation
+    //!
+    template<typename T>
+    void norm2_contract2(const Tensor3<T>& A, const Tensor3<T>& B, Tensor2<T>& C)
+    {
+      const auto r1 = A.r1();
+      const auto n = A.n();
+      const auto nChunks = A.nChunks();
+      const auto r2 = A.r2(); // == B.r1() == B.r2()
+      C.resize(r1,r1);
+
+      const auto timer = PITTS::performance::createScopedTimer<TensorTrain<T>>(
+        {{"r1", "nChunks", "r2"},{r1, nChunks, r2}}, // arguments
+        {{0.5*r1*(r1+1)*nChunks*r2*Chunk<T>::size*kernel_info::FMA<T>()}, // flops
+         {(r1*nChunks*r2+r1*nChunks*r2)*kernel_info::Load<Chunk<T>>() + (r1*r1)*kernel_info::Store<Chunk<T>>()}} // data transfers
+        );
+
+      double tmpC[r1*r1];
+      for(int i = 0; i < r1; i++)
+        for(int j = 0; j < r1; j++)
+          tmpC[i*r1+j] = 0;
+
+#pragma omp parallel reduction(+:tmpC)
+{
+      for(int j = 0; j < r1; j++)
+        for(int i = j; i < r1; i++)
+        {
+          Chunk<T> tmp{};
+#pragma omp for schedule(static) nowait
+          for(int kChunk = 0; kChunk < nChunks; kChunk++)
+            for(int l = 0; l < r2; l++)
+              fmadd(A.chunk(i,kChunk,l), B.chunk(j,kChunk,l), tmp);
+          tmpC[i+j*r1] = tmpC[j+i*r1] = sum(tmp);
+        }
+}
+      for(int i = 0; i < r1; i++)
+        for(int j = 0; j < r1; j++)
+          C(i,j) = tmpC[i*r1+j];
+
+    }
+  }
+
   //! calculate the 2-norm for a vector in tensor train format
   //!
   //! @tparam T  underlying data type (double, complex, ...)
@@ -56,28 +134,10 @@ namespace PITTS
       const auto n = subT.n();
 
       // first contraction: subT(:,:,*) * t2(:,*)
-      t3.resize(r1, n, r2);
-      for(int i = 0; i < r1; i++)
-        for(int j = 0; j < n; j++)
-          for(int k = 0; k < r2; k++)
-          {
-            T tmp{};
-            for(int l = 0; l < r2; l++)
-              tmp += subT(i,j,l) * t2(k,l);
-            t3(i,j,k) = tmp;
-          }
+      internal::norm2_contract1(subT, t2, t3);
 
       // second contraction: subT(:,*,*) * t3(:,*,*)
-      t2.resize(r1,r1);
-      for(int i = 0; i < r1; i++)
-        for(int j = 0; j < r1; j++)
-        {
-          T tmp{};
-          for(int k = 0; k < n; k++)
-            for(int l = 0; l < r2; l++)
-              tmp += subT(i,k,l) * t3(j,k,l);
-          t2(i,j) = tmp;
-        }
+      internal::norm2_contract2(subT, t3, t2);
     }
 
     return std::sqrt(t2(0,0));
