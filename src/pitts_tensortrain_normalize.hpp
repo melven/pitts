e965b4320958c25d2d2b2e4a958196f083e40f7d
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2021-06-09 16:12:04 +0200

    tensortrain: try to use Eigen::BDCSVD again in axpby

--- src/pitts_tensortrain_normalize.hpp
+++ src/pitts_tensortrain_normalize.hpp
@@ -130,8 +130,8 @@ namespace PITTS
             t2_M(i+j*r1_new,k) = t3_tmp(i,j,k);
 
       using EigenMatrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
-      //Eigen::BDCSVD<EigenMatrix> svd(ConstEigenMap(t2_M), Eigen::ComputeThinU | Eigen::ComputeThinV);
-      Eigen::JacobiSVD<EigenMatrix> svd(ConstEigenMap(t2_M), Eigen::ComputeThinU | Eigen::ComputeThinV);
+      Eigen::BDCSVD<EigenMatrix> svd(ConstEigenMap(t2_M), Eigen::ComputeThinU | Eigen::ComputeThinV);
+      //Eigen::JacobiSVD<EigenMatrix> svd(ConstEigenMap(t2_M), Eigen::ComputeThinU | Eigen::ComputeThinV);
       svd.setThreshold(rankTolerance / std::sqrt(T(nDims-1)));
       //std::cout << "singularValues: " << svd.singularValues().transpose() << "\n";
       const auto r2_new = std::min<int>(maxRank, svd.rank());
