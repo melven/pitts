df4235e4a5bb59d7aca897535e4a45b65031641a
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2021-06-09 12:21:01 +0200

    tensortrain: faster/parallel TT-dot implementation

--- src/dot_bench.cpp
+++ src/dot_bench.cpp
@@ -11,14 +11,14 @@ int main(int argc, char* argv[])
   PITTS::initialize(&argc, &argv);
 
   using Type = double;
-  PITTS::TensorTrain<Type> TT1(10,100), TT2(10,100);
+  PITTS::TensorTrain<Type> TT1(10,10000), TT2(10,10000);
   const int r = 20;
   TT1.setTTranks({r,r,r,r,r,r,r,r,r});
   TT2.setTTranks({r,r,r,r,r,r,r,r,r});
   randomize(TT1);
   randomize(TT2);
   Type tmp = 0;
-  for(int iter = 0; iter < 1000; iter++)
+  for(int iter = 0; iter < 100; iter++)
   {
     tmp += dot(TT1,TT2);
   }
