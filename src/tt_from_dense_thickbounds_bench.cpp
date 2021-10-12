81cbeeb773d85daa1164ffe83765b94f579d0f6a
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2021-10-12 13:41:49 +0200

    benchmarks: allow better weak scaling tests

--- src/tt_from_dense_thickbounds_bench.cpp
+++ src/tt_from_dense_thickbounds_bench.cpp
@@ -13,8 +13,8 @@ int main(int argc, char* argv[])
 {
   PITTS::initialize(&argc, &argv);
 
-  if( argc != 5 && argc != 6 )
-    throw std::invalid_argument("Requires 4 or 5 arguments (n d max_r nIter [f_bound])!");
+  if( argc < 5 || argc > 7 )
+    throw std::invalid_argument("Requires 4 to 6 arguments (n d max_r nIter [f_bound] [localDims])!");
 
   std::size_t n = 0, d = 0, max_r = 0, nIter = 0;
   std::from_chars(argv[1], argv[2], n);
@@ -22,14 +22,19 @@ int main(int argc, char* argv[])
   std::from_chars(argv[3], argv[4], max_r);
   std::from_chars(argv[4], argv[5], nIter);
   std::size_t f_bound = 2;
-  if( argc == 6 )
+  if( argc >= 6 )
     std::from_chars(argv[5], argv[6], f_bound);
+  int localDims = false;
+  if( argc >= 7 )
+    std::from_chars(argv[6], argv[7], localDims);
 
   const auto& [iProc,nProcs] = PITTS::internal::parallel::mpiProcInfo();
 
   // compress shape, s.t. first and last dimensions are bigger than max_r
   // first dimension is distributed over MPI processes
   std::vector<int> shape(d,n);
+  if( localDims )
+    shape.insert(shape.begin(), nProcs);
   const double nMin = std::max<double>(f_bound*max_r, 10.);
   while( shape.size() > 2 && shape.front() <= nMin*nProcs )
   {
@@ -54,17 +59,27 @@ int main(int argc, char* argv[])
     nTotal *= ni;
 
 
-  PITTS::MultiVector<double> data(nTotal/shape.back(), shape.back());
-  randomize(data);
-
   PITTS::MultiVector<double> X(nTotal/shape.back(), shape.back());
-  PITTS::MultiVector<double> work;
+  PITTS::MultiVector<double> work(nTotal, 1);
+  randomize(work);
 
+
+  double min_wtime = std::numeric_limits<double>::max();
   for(int iter = 0; iter < nIter; iter++)
   {
-    copy(data, X);
+    X.resize(nTotal/shape.back(), shape.back());
+    randomize(X);
+    double wtime = omp_get_wtime();
+
     const auto TT = fromDense(X, work, shape, 1.e-8, max_r, true);
+
+    wtime = omp_get_wtime() - wtime;
+    if( iProc == 0 )
+      std::cout << "wtime: " << wtime << "\n";
+    min_wtime = std::min(min_wtime, wtime);
   }
+  if( iProc == 0 )
+    std::cout << "min. wtime: " << min_wtime << "\n";
 
   PITTS::finalize();
 
