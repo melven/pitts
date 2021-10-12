c68440d1c8dadecc2badb05f1eb76f2c0eba40de
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2021-10-12 13:42:46 +0200

    bench: add new weak and strong-scaling benchmarking results for TT-SVD

---
+++ bench/sc-cluster/scaling2/tt_from_dense_thickbounds_d32_weak_n10.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --exclusive
+#SBATCH -n 10
+#SBATCH -c 14
+#SBATCH --threads-per-core=1
+#SBATCH -t 120
+#SBATCH --output="%x-%j.out"
+
+# module load PrgEnv/gcc10-openmpi-python
+
+export OMP_NUM_THREADS=14
+
+for i in 10 20 30 40 50; do
+  srun --cpu-bind=core,verbose likwid-pin -c 0-13 ../../../build/src/tt_from_dense_thickbounds_bench 2 32 $i 10 2 1
+done
