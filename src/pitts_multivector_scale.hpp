36605dcb8cb344c963514ed295f927db7e11b2c6
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2022-05-13 16:13:17 +0200

    multivector: add scale function

--- src/pitts_multivector_scale.hpp
+++ src/pitts_multivector_scale.hpp
@@ -1,5 +1,5 @@
-/*! @file pitts_multivector_norm.hpp
-* @brief calculate the 2-norm each vector in a multi-vector
+/*! @file pitts_multivector_scale.hpp
+* @brief scale each column in a multi-vector with a scalar
 * @author Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
 * @date 2022-05-13
 * @copyright Deutsches Zentrum fuer Luft- und Raumfahrt e. V. (DLR), German Aerospace Center
@@ -7,8 +7,8 @@
 **/
 
 // include guard
-#ifndef PITTS_MULTIVECTOR_NORM_HPP
-#define PITTS_MULTIVECTOR_NORM_HPP
+#ifndef PITTS_MULTIVECTOR_SCALE_HPP
+#define PITTS_MULTIVECTOR_SCALE_HPP
 
 // includes
 #include <array>
@@ -24,17 +24,17 @@
 //! namespace for the library PITTS (parallel iterative tensor train solvers)
 namespace PITTS
 {
-  //! calculate the 2-norm of the columns of a multi-vector
+  //! scale each column in a multi-vector with a scalar
   //!
-  //! alpha_i <- sqrt(X(:,i)^T X(:,i))
+  //! X(:,i) <- alpha_i * X(:,i)
   //!
   //! @tparam T       underlying data type (double, complex, ...)
   //!
+  //! @param alpha    array of scaling factors, dimension (m)
   //! @param X        input multi-vector, dimensions (n, m)
-  //! @return         array of norms (m)
   //!
   template<typename T>
-  auto norm2(const MultiVector<T>& X)
+  void scale(const Eigen::Array<T,Eigen::Dynamic,1>& alpha, MultiVector<T>& X)
   {
     const auto nChunks = X.rowChunks();
     const auto nCols = X.cols();
@@ -42,29 +42,22 @@ namespace PITTS
     // gather performance data
     const auto timer = PITTS::performance::createScopedTimer<MultiVector<T>>(
         {{"nChunks", "nCols"},{nChunks, nCols}}, // arguments
-        {{nCols*nChunks*Chunk<T>::size*kernel_info::FMA<T>()}, // flops
-         {nChunks*nCols*kernel_info::Load<Chunk<T>>() + nCols*kernel_info::Store<T>()}} // data transfers
+        {{nCols*nChunks*Chunk<T>::size*kernel_info::Mult<T>()}, // flops
+         {nCols*kernel_info::Load<T>() + nChunks*nCols*kernel_info::Update<Chunk<T>>()}} // data transfers
         );
 
-    T tmp[nCols] = {};
-#pragma omp parallel reduction(+:tmp)
+#pragma omp parallel
     {
       for(int iCol = 0; iCol < nCols; iCol++)
       {
-        Chunk<T> tmpChunk{};
 #pragma omp for schedule(static) nowait
         for(int iChunk = 0; iChunk < nChunks; iChunk++)
-          fmadd(X.chunk(iChunk,iCol), X.chunk(iChunk,iCol), tmpChunk);
-        tmp[iCol] = sum(tmpChunk);
+          mul(alpha(iCol), X.chunk(iChunk,iCol), X.chunk(iChunk,iCol));
       }
     }
-
-    using arr = Eigen::Array<T, Eigen::Dynamic, 1>;
-    arr result = Eigen::Map<arr>(tmp, nCols).sqrt();
-    return result;
   }
 
 }
 
 
-#endif // PITTS_MULTIVECTOR_NORM_HPP
+#endif // PITTS_MULTIVECTOR_SCALE_HPP
