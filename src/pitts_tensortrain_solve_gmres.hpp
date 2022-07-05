66d8296187bea99f7e33f19fe37e8454371f5c87
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2022-07-05 13:32:45 +0200

    tt_gmres: first generic TT-GMRES implementation

---
+++ src/pitts_tensortrain_solve_gmres.hpp
@@ -0,0 +1,160 @@
+/*! @file pitts_tensortrain_solve_gmres.hpp
+* @brief TT-GMRES algorithm, iterative solver for linear systems in tensor-train format
+* @author Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
+* @date 2022-07-04
+* @copyright Deutsches Zentrum fuer Luft- und Raumfahrt e. V. (DLR), German Aerospace Center
+*
+**/
+
+// include guard
+#ifndef PITTS_TENSORTRAIN_SOLVE_GMRES_HPP
+#define PITTS_TENSORTRAIN_SOLVE_GMRES_HPP
+
+// includes
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include "pitts_tensortrain.hpp"
+#include "pitts_tensortrain_norm.hpp"
+#include "pitts_tensortrain_axpby.hpp"
+#include "pitts_tensortrain_operator.hpp"
+#include "pitts_tensortrain_operator_apply.hpp"
+#include "pitts_tensortrain_gram_schmidt.hpp"
+#include "pitts_gmres.hpp"
+#include "pitts_timer.hpp"
+#pragma GCC push_options
+#pragma GCC optimize("no-unsafe-math-optimizations")
+#include <Eigen/Dense>
+#pragma GCC pop_options
+
+//! namespace for the library PITTS (parallel iterative tensor train solvers)
+namespace PITTS
+{
+  //! namespace for helper functionality
+  namespace internal
+  {
+    //! helper function for converting an array to a string
+    template<typename T>
+    std::string to_string(const std::vector<T>& v)
+    {
+      std::string result = "[";
+      for(int i = 0; i < v.size(); i++)
+      {
+        if( i > 0 )
+          result += ", ";
+        result += std::to_string(v[i]);
+      }
+      result += "]";
+      return result;
+    }
+  }
+
+  //! TT-GMRES: iterative solver for linear systems in tensor-train format
+  //!
+  //! Given A, b, calculates x with Ax=b approximately up to a given tolerance.
+  //!
+  //! @tparam T             data type (double, float, complex)
+  //!
+  //! @param TTOpA              tensor-train operator A
+  //! @param TTb                right-hand side tensor-train b
+  //! @param TTx                initial guess on input, overwritten with the (approximate) result on output
+  //! @param maxIter            maximal number of iterations
+  //! @param absResTol          absolute residual tolerance: the iteration aborts if the absolute residual norm is smaller than absResTol
+  //! @param relResTol          relative residual tolerance: the iteration aborts if the relative residual norm is smaller than relResTol
+  //! @param maxRank            maximal allowed TT-rank, enforced even if this violates the residualTolerance
+  //! @param adaptiveTolerance  use an adaptive tolerance for the tensor-train arithmetic in the iteration
+  //! @param outputPrefix       string to prefix all output about the convergence history
+  //! @param verbose            set to true, to print the residual norm in each iteration to std::cout
+  //! @return                   residual norm of the result (||Ax - b||)
+  //!
+  template <typename T>
+  auto solveGMRES(const TensorTrainOperator<T> &TTOpA, const TensorTrain<T> &TTb, TensorTrain<T> &TTx,
+                  int maxIter, T absResTol, T relResTol,
+                  int maxRank = std::numeric_limits<int>::max(), bool adaptiveTolerance = true,
+                  const std::string &outputPrefix = "", bool verbose = false)
+  {
+    using vec = Eigen::Matrix<T,Eigen::Dynamic,1>;
+    using mat = Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic>;
+
+    const auto timer = PITTS::timing::createScopedTimer<TensorTrain<T>>();
+
+    std::vector<TensorTrain<T>> V;
+    // calculate the initial residual
+    V.emplace_back(TensorTrain<T>{TTOpA.row_dimensions()});
+    apply(TTOpA, TTx, V[0]);
+    const T beta = axpby(T(-1), TTb, T(1), V[0]);
+    // RHS norm for the relative error
+    const T nrm_b = norm2(TTb);
+
+    if( verbose )
+      std::cout << outputPrefix << "Initial residual norm: " << beta << " (abs), " << beta / nrm_b << " (rel), ranks: " << internal::to_string(TTx.getTTranks()) << "\n";
+
+    // relative residual tolerance used below also for calculating the required TT rankTolerance
+    const T residualTolerance = std::max(relResTol, absResTol/nrm_b);
+    if( beta <= residualTolerance * nrm_b )
+      return beta;
+
+    vec b_hat = vec::Zero(maxIter+1);
+    b_hat(0) = beta;
+    mat H = mat::Zero(maxIter+1, maxIter);
+    mat R = mat::Zero(maxIter, maxIter);
+    vec c = vec::Zero(maxIter);
+    vec s = vec::Zero(maxIter);
+
+    // current residual
+    T rho = beta;
+
+    for(int i = 0; i < maxIter; i++)
+    {
+      TensorTrain<T> w(TTOpA.row_dimensions());
+      apply(TTOpA, V[i], w);
+
+      T rankTolerance = residualTolerance / maxIter;
+      if( adaptiveTolerance )
+        rankTolerance *= beta / rho;
+
+      H.col(i).segment(0, i+2) = gramSchmidt(V, w, rankTolerance, maxRank, outputPrefix + "  gramSchmidt: ", verbose);
+
+      // least squares solve using Givens rotations
+      R(0,i) = H(0,i);
+      for(int j = 1; j <= i; j++)
+      {
+        const T gamma = c(j-1)*R(j-1,i) + s(j-1)*H(j,i);
+        R(j,i) = -s(j-1)*R(j-1,i) + c(j-1)*H(j,i);
+        R(j-1,i) = gamma;
+      }
+      const T delta = std::sqrt(R(i,i)*R(i,i) + H(i+1,i)*H(i+1,i));
+      c(i) = R(i,i) / delta;
+      s(i) = H(i+1,i) / delta;
+      R(i,i) = c(i)*R(i,i) + s(i)*H(i+1,i);
+      b_hat(i+1) = -s(i)*b_hat(i);
+      b_hat(i) = c(i)*b_hat(i);
+      rho = std::abs(b_hat(i+1));
+      if( verbose )
+        std::cout << outputPrefix << "TT-GMRES iteration " << i+1 << " residual norm: " << rho << " (abs), " << rho / nrm_b << " (rel), ranks: " << internal::to_string(TTx.getTTranks()) << "\n";
+
+      // check convergence
+      if( rho <= residualTolerance * nrm_b )
+        break;
+    }
+
+    // calculate solution
+    const int m = V.size() - 1;
+    const mat Ri = R.topLeftCorner(m, m);
+    const vec bi = b_hat.topRows(m);
+    const vec y = internal::backwardSolve(Ri, bi);
+    T nrm_x = 1;
+    for(int j = 0; j < m; j++)
+    {
+      const T rankTolerance = residualTolerance / m;
+      nrm_x = axpby(T(-y(j)), V[j], nrm_x, TTx, rankTolerance, maxRank);
+    }
+    internal::t3_scale(nrm_x, TTx.editableSubTensors()[0]);
+
+    return rho;
+  }
+
+}
+
+
+#endif // PITTS_TENSORTRAIN_SOLVE_GMRES_HPP
---
+++ test/test_tensortrain_solve_gmres.cpp
@@ -0,0 +1,220 @@
+#include <gtest/gtest.h>
+#include "pitts_tensortrain_solve_gmres.hpp"
+#include "pitts_tensortrain_random.hpp"
+#include "pitts_tensortrain_operator_apply_transposed_op.hpp"
+#include "pitts_tensortrain_operator_apply_transposed.hpp"
+#include "pitts_tensortrain_to_dense.hpp"
+#include "eigen_test_helper.hpp"
+
+namespace
+{
+  using TensorTrain_double = PITTS::TensorTrain<double>;
+  using TensorTrainOperator_double = PITTS::TensorTrainOperator<double>;
+  constexpr auto eps = 1.e-7;
+}
+
+TEST(PITTS_TensorTrain_solve_gmres, Opeye_ones_nDim1)
+{
+  TensorTrainOperator_double TTOpA(1,5,5);
+  TTOpA.setEye();
+  TensorTrain_double TTx(1,5), TTb(1,5);
+  TTb.setOnes();
+  TTx.setOnes();
+
+  double error = solveGMRES(TTOpA, TTb, TTx, 0, eps, eps, 999, true, "test: ", true);
+  EXPECT_NEAR(0, error, eps);
+
+  double errNrm = axpby(-1., TTb, 1., TTx);
+  EXPECT_NEAR(0, errNrm, eps);
+}
+
+TEST(PITTS_TensorTrain_solve_gmres, Opeye_ones_nDim2_guess_zero)
+{
+  TensorTrainOperator_double TTOpA(2,5,5);
+  TTOpA.setEye();
+  TensorTrain_double TTx(2,5), TTb(2,5);
+  TTb.setOnes();
+  TTx.setZero();
+
+  double error = solveGMRES(TTOpA, TTb, TTx, 1, eps, eps, 999, true, "test: ", true);
+  EXPECT_NEAR(0, error, eps);
+
+  double errNrm = axpby(-1., TTb, 1., TTx);
+  EXPECT_NEAR(0, errNrm, eps);
+}
+
+TEST(PITTS_TensorTrain_solve_gmres, random_nDim1)
+{
+  TensorTrainOperator_double TTOpA(1,5,5);
+  randomize(TTOpA);
+  TensorTrain_double TTx(1,5), TTb(1,5);
+  randomize(TTb);
+  randomize(TTx);
+
+  double error = solveGMRES(TTOpA, TTb, TTx, 5, eps, eps, 999, true, "test: ", true);
+  EXPECT_NEAR(0, error, eps);
+
+  TensorTrain_double TTAx(TTb.dimensions());
+  apply(TTOpA, TTx, TTAx);
+  double error_ref = axpby(-1., TTb, 1., TTAx);
+  EXPECT_NEAR(error_ref, error, eps);
+}
+
+TEST(PITTS_TensorTrain_solve_gmres, symmetric_random_nDim1)
+{
+  TensorTrainOperator_double TTOp_tmp(1,5,5);
+  randomize(TTOp_tmp);
+  TensorTrainOperator_double TTOpA(1,5,5);
+  applyT(TTOp_tmp, TTOp_tmp, TTOpA);
+  TensorTrain_double TTx(1,5), TTb(1,5);
+  randomize(TTb);
+  randomize(TTx);
+
+  double error = solveGMRES(TTOpA, TTb, TTx, 5, eps, eps, 999, true, "test: ", true);
+  EXPECT_NEAR(0, error, eps);
+
+  TensorTrain_double TTAx(TTb.dimensions());
+  apply(TTOpA, TTx, TTAx);
+  double error_ref = axpby(-1., TTb, 1., TTAx);
+  EXPECT_NEAR(error_ref, error, eps);
+}
+
+TEST(PITTS_TensorTrain_solve_gmres, random_nDim2_rank1)
+{
+  TensorTrainOperator_double TTOpA(2,5,5);
+  TTOpA.setTTranks(1);
+  randomize(TTOpA);
+  // make it diagonally dominant to obtain a well-posed problem
+  for(int iDim = 0; iDim < 2; iDim++)
+  {
+    auto& subT = TTOpA.tensorTrain().editableSubTensors()[iDim];
+    for(int i = 0; i < 5; i++)
+      subT(0, TTOpA.index(iDim, i, i), 0) += 4;
+  }
+  TensorTrain_double TTx(2,5), TTb(2,5);
+  TTb.setTTranks(1);
+  randomize(TTb);
+  normalize(TTb);
+  randomize(TTx);
+  normalize(TTx);
+
+  double error = solveGMRES(TTOpA, TTb, TTx, 25, eps, eps, 999, true, "test: ", true);
+  EXPECT_NEAR(0, error, 100*eps);
+
+  TensorTrain_double TTAx(TTb.dimensions());
+  apply(TTOpA, TTx, TTAx);
+  double error_ref = axpby(-1., TTb, 1., TTAx);
+  EXPECT_NEAR(error_ref, error, eps);
+}
+
+TEST(PITTS_TensorTrain_solve_gmres, random_nDim2)
+{
+  TensorTrainOperator_double TTOpA(2,2,2);
+  TTOpA.setTTranks(2);
+  randomize(TTOpA);
+  normalize(TTOpA);
+  TensorTrainOperator_double TTOpI(2,2,2);
+  TTOpI.setEye();
+  const double Inrm = normalize(TTOpI);
+  axpby(Inrm, TTOpI, Inrm/10, TTOpA);
+
+  TensorTrain_double TTx(2,2), TTb(2,2);
+  TTb.setTTranks(2);
+  randomize(TTb);
+  TTx.setOnes();
+
+  double error = solveGMRES(TTOpA, TTb, TTx, 4, eps, eps, 999, true, "test: ", true);
+  EXPECT_NEAR(0, error, 1.e-5*norm2(TTb));
+
+  TensorTrain_double TTAx(TTb.dimensions());
+  apply(TTOpA, TTx, TTAx);
+  double error_ref = axpby(-1., TTb, 1., TTAx);
+  EXPECT_NEAR(error_ref, error, 1.e-5*norm2(TTb));
+}
+
+TEST(PITTS_TensorTrain_solve_gmres, symmetric_random_nDim6_rank1)
+{
+  TensorTrainOperator_double TTOp_tmp(6,5,4);
+  TTOp_tmp.setTTranks(1);
+  randomize(TTOp_tmp);
+  TensorTrainOperator_double TTOpA(6,4,4);
+  applyT(TTOp_tmp, TTOp_tmp, TTOpA);
+  // make it diagonally dominant to obtain a well-posed problem
+  for(int iDim = 0; iDim < 6; iDim++)
+  {
+    auto& subT = TTOpA.tensorTrain().editableSubTensors()[iDim];
+    for(int i = 0; i < 4; i++)
+      subT(0, TTOpA.index(iDim, i, i), 0) += 50;
+  }
+
+  TensorTrain_double TTx(6,4), TTb(6,4), TTx_ref(6,4), TTr(6,4), TTdx(6,4);
+  TTx_ref.setTTranks(1);
+  randomize(TTx_ref);
+  apply(TTOpA, TTx_ref, TTb);
+
+  TTx.setOnes();
+
+  copy(TTx, TTdx);
+  double initialError = axpby(-1., TTx_ref, 1., TTdx);
+  apply(TTOpA, TTx, TTr);
+  double initialResidualNorm = axpby(-1., TTb, 1., TTr);
+
+
+  double residualNorm = solveGMRES(TTOpA, TTb, TTx, 25, 0.01, 0.01, 999, true, "test: ", true);
+
+
+  apply(TTOpA, TTx, TTr);
+  double residualNorm_ref = axpby(-1., TTb, 1., TTr);
+  EXPECT_NEAR(residualNorm_ref, residualNorm, 0.001*initialResidualNorm);
+
+  std::cout << "initialResidualNorm: " << initialResidualNorm << ", newResidualNorm: " << residualNorm << "\n";
+
+  copy(TTx, TTdx);
+  double error = axpby(-1., TTx_ref, 1., TTdx);
+  std::cout << "initialError: " << initialError << ", newError: " << error << "\n";
+  EXPECT_NEAR(0, error/initialError, 0.01);
+}
+
+TEST(PITTS_TensorTrain_solve_gmres, symmetric_random_nDim6)
+{
+  TensorTrainOperator_double TTOp_tmp(6,5,4);
+  TTOp_tmp.setTTranks(2);
+  randomize(TTOp_tmp);
+  TensorTrainOperator_double TTOpA(6,4,4);
+  applyT(TTOp_tmp, TTOp_tmp, TTOpA);
+  // make it diagonally dominant to obtain a well-posed problem
+  for(int iDim = 0; iDim < 6; iDim++)
+  {
+    auto& subT = TTOpA.tensorTrain().editableSubTensors()[iDim];
+    for(int i = 0; i < 4; i++)
+      subT(0, TTOpA.index(iDim, i, i), 0) += 50;
+  }
+
+  TensorTrain_double TTx(6,4), TTb(6,4), TTx_ref(6,4), TTr(6,4), TTdx(6,4);
+  TTx_ref.setTTranks(2);
+  randomize(TTx_ref);
+  apply(TTOpA, TTx_ref, TTb);
+
+  TTx.setTTranks(3);
+  randomize(TTx);
+
+  copy(TTx, TTdx);
+  double initialError = axpby(-1., TTx_ref, 1., TTdx);
+  apply(TTOpA, TTx, TTr);
+  double initialResidualNorm = axpby(-1., TTb, 1., TTr);
+
+
+  double residualNorm = solveGMRES(TTOpA, TTb, TTx, 25, 0.01, 0.01, 999, true, "test: ", true);
+
+
+  apply(TTOpA, TTx, TTr);
+  double residualNorm_ref = axpby(-1., TTb, 1., TTr);
+  EXPECT_NEAR(residualNorm_ref, residualNorm, 0.01*initialResidualNorm);
+
+  std::cout << "initialResidualNorm: " << initialResidualNorm << ", newResidualNorm: " << residualNorm << "\n";
+
+  copy(TTx, TTdx);
+  double error = axpby(-1., TTx_ref, 1., TTdx);
+  std::cout << "initialError: " << initialError << ", newError: " << error << "\n";
+  EXPECT_NEAR(0, error/initialError, 0.01);
+}
