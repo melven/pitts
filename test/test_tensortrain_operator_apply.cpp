429d4701cf78ab901b0bab478642eb0536f21725
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2022-07-18 14:29:15 +0200

    tt_operator: apply with boundary rank

--- test/test_tensortrain_operator_apply.cpp
+++ test/test_tensortrain_operator_apply.cpp
@@ -3,6 +3,11 @@
 #include "pitts_tensortrain_random.hpp"
 #include "pitts_tensortrain_axpby.hpp"
 #include "pitts_tensortrain_laplace_operator.hpp"
+#include "pitts_tensortrain_to_dense.hpp"
+#include "pitts_tensortrain_operator_apply_dense.hpp"
+#include "pitts_multivector.hpp"
+#include "pitts_multivector_eigen_adaptor.hpp"
+#include "eigen_test_helper.hpp"
 
 TEST(PITTS_TensorTrainOperator_apply, zero)
 {
@@ -118,3 +123,90 @@ TEST(PITTS_TensorTrainOperator_apply, laplace_operator)
   EXPECT_NEAR(0., error, eps);
 }
 
+TEST(PITTS_TensorTrainOperator_apply, eye_boundaryRank)
+{
+  using TensorTrainOperator_double = PITTS::TensorTrainOperator<double>;
+  using TensorTrain_double = PITTS::TensorTrain<double>;
+  constexpr auto eps = 1.e-10;
+
+  TensorTrainOperator_double TTOp({3,4,2,5}, {3,4,2,5});
+  TTOp.setEye();
+
+  TensorTrain_double TTx(std::vector<int>{4,2}), TTy(std::vector<int>{4,2});
+  TTx.editableSubTensors()[0].resize(3,4,2);
+  TTx.editableSubTensors()[1].resize(2,2,5);
+  randomize(TTx);
+
+  apply(TTOp, TTx, TTy);
+
+  ASSERT_EQ(TTx.getTTranks(), TTy.getTTranks());
+  for(int iDim = 0; iDim < TTx.subTensors().size(); iDim++)
+  {
+    const auto& subTx = TTx.subTensors()[iDim];
+    const auto& subTy = TTy.subTensors()[iDim];
+    for(int i = 0; i < subTy.r1(); i++)
+      for(int j = 0; j < subTy.n(); j++)
+        for(int k = 0; k < subTy.r2(); k++)
+          ASSERT_NEAR(subTx(i,j,k), subTy(i,j,k), eps);
+  }
+}
+
+TEST(PITTS_TensorTrainOperator_apply, random_boundaryRank_rank1)
+{
+  using TensorTrainOperator_double = PITTS::TensorTrainOperator<double>;
+  using TensorTrain_double = PITTS::TensorTrain<double>;
+  using MultiVector_double = PITTS::MultiVector<double>;
+  constexpr auto eps = 1.e-10;
+
+  TensorTrainOperator_double TTOp({3,4,2,5}, {2,4,2,3});
+  TTOp.setTTranks(1);
+  randomize(TTOp);
+
+  TensorTrain_double TTx(std::vector<int>{4,2}), TTy(std::vector<int>{4,2});
+  TTx.editableSubTensors()[0].resize(2,4,1);
+  TTx.editableSubTensors()[1].resize(1,2,3);
+  randomize(TTx);
+
+  apply(TTOp, TTx, TTy);
+
+  ASSERT_EQ(3, TTy.subTensors()[0].r1());
+  ASSERT_EQ(5, TTy.subTensors()[1].r2());
+
+  // calculate a reference solution using TTOp * dense and toDense
+  MultiVector_double MVx, MVy_ref, MVy;
+  toDense(TTx, MVx);
+  apply(TTOp, MVx, MVy_ref);
+  toDense(TTy, MVy);
+
+  EXPECT_NEAR(ConstEigenMap(MVy_ref), ConstEigenMap(MVy), eps);
+}
+
+TEST(PITTS_TensorTrainOperator_apply, random_boundaryRank)
+{
+  using TensorTrainOperator_double = PITTS::TensorTrainOperator<double>;
+  using TensorTrain_double = PITTS::TensorTrain<double>;
+  using MultiVector_double = PITTS::MultiVector<double>;
+  constexpr auto eps = 1.e-10;
+
+  TensorTrainOperator_double TTOp({3,4,2,5}, {2,4,2,3});
+  TTOp.setTTranks(2);
+  randomize(TTOp);
+
+  TensorTrain_double TTx(std::vector<int>{4,2}), TTy(std::vector<int>{4,2});
+  TTx.editableSubTensors()[0].resize(2,4,6);
+  TTx.editableSubTensors()[1].resize(6,2,3);
+  randomize(TTx);
+
+  apply(TTOp, TTx, TTy);
+
+  ASSERT_EQ(3, TTy.subTensors()[0].r1());
+  ASSERT_EQ(5, TTy.subTensors()[1].r2());
+
+  // calculate a reference solution using TTOp * dense and toDense
+  MultiVector_double MVx, MVy_ref, MVy;
+  toDense(TTx, MVx);
+  apply(TTOp, MVx, MVy_ref);
+  toDense(TTy, MVy);
+
+  EXPECT_NEAR(ConstEigenMap(MVy_ref), ConstEigenMap(MVy), eps);
+}
