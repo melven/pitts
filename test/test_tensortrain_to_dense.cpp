34d144b4e7cecfc5be9111de6ee1319d2b95af60
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2022-09-25 10:40:39 +0200

    tt_refactoring: use new access functions in unit test

--- test/test_tensortrain_to_dense.cpp
+++ test/test_tensortrain_to_dense.cpp
@@ -8,11 +8,14 @@
 TEST(PITTS_TensorTrain_toDense, scalar)
 {
   using TensorTrain_double = PITTS::TensorTrain<double>;
+  using Tensor3_double = PITTS::Tensor3<double>;
   constexpr auto eps = 1.e-10;
 
+  Tensor3_double subT(1,1,1);
+  subT(0,0,0) = 5.;
   const std::vector<int> dimensions = {1};
   TensorTrain_double TT(dimensions);
-  TT.editableSubTensors()[0](0,0,0) = 5.;
+  TT.setSubTensor(0, std::move(subT));
 
   std::array<double,1> scalar;
   PITTS::toDense(TT, begin(scalar), end(scalar));
@@ -42,15 +45,18 @@ TEST(PITTS_TensorTrain_toDense, dimension_mismatch)
 TEST(PITTS_TensorTrain_toDense, vector)
 {
   using TensorTrain_double = PITTS::TensorTrain<double>;
+  using Tensor3_double = PITTS::Tensor3<double>;
   constexpr auto eps = 1.e-10;
 
   const std::vector<int> dimensions = {5};
   TensorTrain_double TT(dimensions);
-  TT.editableSubTensors()[0](0,0,0) = 1.;
-  TT.editableSubTensors()[0](0,1,0) = 2.;
-  TT.editableSubTensors()[0](0,2,0) = 3.;
-  TT.editableSubTensors()[0](0,3,0) = 4.;
-  TT.editableSubTensors()[0](0,4,0) = 5.;
+  Tensor3_double subT(1,5,1);
+  subT(0,0,0) = 1.;
+  subT(0,1,0) = 2.;
+  subT(0,2,0) = 3.;
+  subT(0,3,0) = 4.;
+  subT(0,4,0) = 5.;
+  TT.setSubTensor(0, std::move(subT));
 
   std::array<double,5> data;
   PITTS::toDense(TT, begin(data), end(data));
@@ -65,17 +71,20 @@ TEST(PITTS_TensorTrain_toDense, vector)
 TEST(PITTS_TensorTrain_toDense, matrix_2d_5x2_rank1)
 {
   using TensorTrain_double = PITTS::TensorTrain<double>;
+  using Tensor3_double = PITTS::Tensor3<double>;
   constexpr auto eps = 1.e-10;
 
-  const std::vector<int> dimensions = {5,2};
-  TensorTrain_double TT(dimensions);
-  TT.editableSubTensors()[0](0,0,0) = 1.;
-  TT.editableSubTensors()[0](0,1,0) = 2.;
-  TT.editableSubTensors()[0](0,2,0) = 3.;
-  TT.editableSubTensors()[0](0,3,0) = 4.;
-  TT.editableSubTensors()[0](0,4,0) = 5.;
-  TT.editableSubTensors()[1](0,0,0) = 1.;
-  TT.editableSubTensors()[1](0,1,0) = 2.;
+  std::vector<Tensor3_double> subTensors(2);
+  subTensors[0].resize(1,5,1);
+  subTensors[0](0,0,0) = 1.;
+  subTensors[0](0,1,0) = 2.;
+  subTensors[0](0,2,0) = 3.;
+  subTensors[0](0,3,0) = 4.;
+  subTensors[0](0,4,0) = 5.;
+  subTensors[1].resize(1,2,1);
+  subTensors[1](0,0,0) = 1.;
+  subTensors[1](0,1,0) = 2.;
+  TensorTrain_double TT(std::move(subTensors));
 
   std::array<double,5*2> data;
   PITTS::toDense(TT, begin(data), end(data));
@@ -95,17 +104,20 @@ TEST(PITTS_TensorTrain_toDense, matrix_2d_5x2_rank1)
 TEST(PITTS_TensorTrain_toDense, matrix_2d_2x5_rank1)
 {
   using TensorTrain_double = PITTS::TensorTrain<double>;
+  using Tensor3_double = PITTS::Tensor3<double>;
   constexpr auto eps = 1.e-10;
 
-  const std::vector<int> dimensions = {2,5};
-  TensorTrain_double TT(dimensions);
-  TT.editableSubTensors()[0](0,0,0) = 1.;
-  TT.editableSubTensors()[0](0,1,0) = 2.;
-  TT.editableSubTensors()[1](0,0,0) = 1.;
-  TT.editableSubTensors()[1](0,1,0) = 2.;
-  TT.editableSubTensors()[1](0,2,0) = 3.;
-  TT.editableSubTensors()[1](0,3,0) = 4.;
-  TT.editableSubTensors()[1](0,4,0) = 5.;
+  std::vector<Tensor3_double> subTensors(2);
+  subTensors[0].resize(1,2,1);
+  subTensors[0](0,0,0) = 1.;
+  subTensors[0](0,1,0) = 2.;
+  subTensors[1].resize(1,5,1);
+  subTensors[1](0,0,0) = 1.;
+  subTensors[1](0,1,0) = 2.;
+  subTensors[1](0,2,0) = 3.;
+  subTensors[1](0,3,0) = 4.;
+  subTensors[1](0,4,0) = 5.;
+  TensorTrain_double TT(std::move(subTensors));
 
   std::array<double,5*2> data;
   PITTS::toDense(TT, begin(data), end(data));
@@ -166,12 +178,14 @@ TEST(PITTS_TensorTrain_toDense, tensor_5d_from_to_dense_random)
 TEST(PITTS_TensorTrain_toDense, boundaryRank_nDim1_ones)
 {
   using TensorTrain_double = PITTS::TensorTrain<double>;
+  using Tensor3_double = PITTS::Tensor3<double>;
   constexpr auto eps = 1.e-8;
 
-  TensorTrain_double TT(1, 7);
-  auto& subT = TT.editableSubTensors()[0];
+  Tensor3_double subT(3,7,5);
   subT.resize(3,7,5);
   subT.setConstant(1);
+  TensorTrain_double TT(1, 7);
+  TT.setSubTensor(0, std::move(subT));
 
   Eigen::VectorXd refData = Eigen::VectorXd::Ones(3*7*5);
 
@@ -185,15 +199,16 @@ TEST(PITTS_TensorTrain_toDense, boundaryRank_nDim1_ones)
 TEST(PITTS_TensorTrain_toDense, boundaryRank_nDim2_ones)
 {
   using TensorTrain_double = PITTS::TensorTrain<double>;
+  using Tensor3_double = PITTS::Tensor3<double>;
   constexpr auto eps = 1.e-8;
 
   TensorTrain_double TT(2, 7);
-  auto& subT_l = TT.editableSubTensors()[0];
-  auto& subT_r = TT.editableSubTensors()[1];
-  subT_l.resize(3,7,1);
-  subT_r.resize(1,7,4);
+  Tensor3_double subT_l(3,7,1);
+  Tensor3_double subT_r(1,7,4);
   subT_l.setConstant(1);
   subT_r.setConstant(1);
+  TT.setSubTensor(0, std::move(subT_l));
+  TT.setSubTensor(1, std::move(subT_r));
 
   Eigen::VectorXd refData = Eigen::VectorXd::Ones(3*7*7*4);
 
@@ -207,16 +222,17 @@ TEST(PITTS_TensorTrain_toDense, boundaryRank_nDim2_ones)
 TEST(PITTS_TensorTrain_toDense, boundaryRank_nDim5_ones)
 {
   using TensorTrain_double = PITTS::TensorTrain<double>;
+  using Tensor3_double = PITTS::Tensor3<double>;
   constexpr auto eps = 1.e-8;
 
   TensorTrain_double TT({3,4,2,2,3});
   TT.setOnes();
-  auto& subT_l = TT.editableSubTensors()[0];
-  auto& subT_r = TT.editableSubTensors()[4];
-  subT_l.resize(5,3,1);
-  subT_r.resize(1,3,2);
+  Tensor3_double subT_l(5,3,1);
+  Tensor3_double subT_r(1,3,2);
   subT_l.setConstant(1);
   subT_r.setConstant(1);
+  TT.setSubTensor(0, std::move(subT_l));
+  TT.setSubTensor(4, std::move(subT_r));
 
   Eigen::VectorXd refData = Eigen::VectorXd::Ones(5 * 3*4*2*2*3 * 2);
 
@@ -230,28 +246,32 @@ TEST(PITTS_TensorTrain_toDense, boundaryRank_nDim5_ones)
 TEST(PITTS_TensorTrain_toDense, boundaryRank_nDim1_random)
 {
   using TensorTrain_double = PITTS::TensorTrain<double>;
+  using Tensor3_double = PITTS::Tensor3<double>;
   constexpr auto eps = 1.e-8;
 
   TensorTrain_double TT(1, 7);
   {
-    auto& subT = TT.editableSubTensors()[0];
-    subT.resize(3,7,5);
+    Tensor3_double subT(3,7,5);
     randomize(subT);
+    TT.setSubTensor(0, std::move(subT));
   }
 
   TensorTrain_double refTT(std::vector<int>{3,7,5});
+  refTT.setTTranks({3,5});
   {
-    copy(TT.subTensors()[0], refTT.editableSubTensors()[1]);
-    auto& subT_l = refTT.editableSubTensors()[0];
-    auto& subT_r = refTT.editableSubTensors()[2];
-    subT_l.resize(1,3,3);
-    subT_r.resize(5,5,1);
+    Tensor3_double subT;
+    copy(TT.subTensor(0), subT);
+    refTT.setSubTensor(1, std::move(subT));
+    Tensor3_double subT_l(1,3,3);
+    Tensor3_double subT_r(5,5,1);
     for(int i = 0; i < 3; i++)
       for(int j = 0; j < 3; j++)
         subT_l(0,i,j) = i == j ? 1 : 0;
     for(int i = 0; i < 5; i++)
       for(int j = 0; j < 5; j++)
         subT_r(i,j,0) = i == j ? 1 : 0;
+    refTT.setSubTensor(0, std::move(subT_l));
+    refTT.setSubTensor(2, std::move(subT_r));
   }
 
   Eigen::VectorXd refData(3*7*5);
@@ -267,32 +287,37 @@ TEST(PITTS_TensorTrain_toDense, boundaryRank_nDim1_random)
 TEST(PITTS_TensorTrain_toDense, boundaryRank_nDim2_random)
 {
   using TensorTrain_double = PITTS::TensorTrain<double>;
+  using Tensor3_double = PITTS::Tensor3<double>;
   constexpr auto eps = 1.e-8;
 
   TensorTrain_double TT(2, 7);
   {
-    auto& subT_l = TT.editableSubTensors()[0];
-    auto& subT_r = TT.editableSubTensors()[1];
-    subT_l.resize(3,7,1);
-    subT_r.resize(1,7,4);
+    Tensor3_double subT_l(3,7,1);
+    Tensor3_double subT_r(1,7,4);
     randomize(subT_l);
     randomize(subT_r);
+    TT.setSubTensor(0, std::move(subT_l));
+    TT.setSubTensor(1, std::move(subT_r));
   }
 
   TensorTrain_double refTT({3,7,7,4});
+  refTT.setTTranks({3,1,4});
   {
-    copy(TT.subTensors()[0], refTT.editableSubTensors()[1]);
-    copy(TT.subTensors()[1], refTT.editableSubTensors()[2]);
-    auto& subT_l = refTT.editableSubTensors()[0];
-    auto& subT_r = refTT.editableSubTensors()[3];
-    subT_l.resize(1,3,3);
-    subT_r.resize(4,4,1);
+    Tensor3_double subT;
+    copy(TT.subTensor(0), subT);
+    subT = refTT.setSubTensor(1, std::move(subT));
+    copy(TT.subTensor(1), subT);
+    subT = refTT.setSubTensor(2, std::move(subT));
+    Tensor3_double subT_l(1,3,3);
+    Tensor3_double subT_r(4,4,1);
     for(int i = 0; i < 3; i++)
       for(int j = 0; j < 3; j++)
         subT_l(0,i,j) = i == j ? 1 : 0;
     for(int i = 0; i < 4; i++)
       for(int j = 0; j < 4; j++)
         subT_r(i,j,0) = i == j ? 1 : 0;
+    refTT.setSubTensor(0, std::move(subT_l));
+    refTT.setSubTensor(3, std::move(subT_r));
   }
 
   Eigen::VectorXd refData(3*7*7*4);
@@ -308,37 +333,45 @@ TEST(PITTS_TensorTrain_toDense, boundaryRank_nDim2_random)
 TEST(PITTS_TensorTrain_toDense, boundaryRank_nDim5_random)
 {
   using TensorTrain_double = PITTS::TensorTrain<double>;
+  using Tensor3_double = PITTS::Tensor3<double>;
   constexpr auto eps = 1.e-8;
 
   TensorTrain_double TT({3,4,2,2,3});
   randomize(TT);
   {
-    auto& subT_l = TT.editableSubTensors()[0];
-    auto& subT_r = TT.editableSubTensors()[4];
-    subT_l.resize(5,3,1);
-    subT_r.resize(1,3,2);
+    Tensor3_double subT_l(5,3,1);
+    Tensor3_double subT_r(1,3,2);
     randomize(subT_l);
     randomize(subT_r);
+    TT.setSubTensor(0, std::move(subT_l));
+    TT.setSubTensor(4, std::move(subT_r));
   }
 
 
   TensorTrain_double refTT({5, 3,4,2,2,3, 2});
+  refTT.setTTranks({5,1,1,1,1,2});
   {
-    copy(TT.subTensors()[0], refTT.editableSubTensors()[1]);
-    copy(TT.subTensors()[1], refTT.editableSubTensors()[2]);
-    copy(TT.subTensors()[2], refTT.editableSubTensors()[3]);
-    copy(TT.subTensors()[3], refTT.editableSubTensors()[4]);
-    copy(TT.subTensors()[4], refTT.editableSubTensors()[5]);
-    auto& subT_l = refTT.editableSubTensors()[0];
-    auto& subT_r = refTT.editableSubTensors()[6];
-    subT_l.resize(1,5,5);
-    subT_r.resize(2,2,1);
+    Tensor3_double subT;
+    copy(TT.subTensor(0), subT);
+    subT = refTT.setSubTensor(1, std::move(subT));
+    copy(TT.subTensor(1), subT);
+    subT = refTT.setSubTensor(2, std::move(subT));
+    copy(TT.subTensor(2), subT);
+    subT = refTT.setSubTensor(3, std::move(subT));
+    copy(TT.subTensor(3), subT);
+    subT = refTT.setSubTensor(4, std::move(subT));
+    copy(TT.subTensor(4), subT);
+    subT = refTT.setSubTensor(5, std::move(subT));
+    Tensor3_double subT_l(1,5,5);
+    Tensor3_double subT_r(2,2,1);
     for(int i = 0; i < 5; i++)
       for(int j = 0; j < 5; j++)
         subT_l(0,i,j) = i == j ? 1 : 0;
     for(int i = 0; i < 2; i++)
       for(int j = 0; j < 2; j++)
         subT_r(i,j,0) = i == j ? 1 : 0;
+    refTT.setSubTensor(0, std::move(subT_l));
+    refTT.setSubTensor(6, std::move(subT_r));
   }
 
   Eigen::VectorXd refData(5 * 3*4*2*2*3 * 2);
