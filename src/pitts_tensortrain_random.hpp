429f0e14d51257cb2ec1f8c9e2107287132bb7b2
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2022-10-23 12:34:37 +0200

    tt_refactoring: use new sub-tensor access functions

--- src/pitts_tensortrain_random.hpp
+++ src/pitts_tensortrain_random.hpp
@@ -25,8 +25,8 @@ namespace PITTS
   template<typename T>
   void randomize(TensorTrain<T>& TT)
   {
-    for(int i = 0; i < TT.dimensions().size(); i++)
-      randomize(TT.editableSubTensor(i));
+    for(auto& subT: TT.subTensors_)
+      randomize(subT);
   }
 
 }
