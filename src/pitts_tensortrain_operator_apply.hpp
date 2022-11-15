8350814f0fc545978a6b54964768a96d9e46ec1a
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2022-11-15 09:48:57 +0100

    tt_op: improve performance of apply

--- src/pitts_tensortrain_operator_apply.hpp
+++ src/pitts_tensortrain_operator_apply.hpp
@@ -34,6 +34,7 @@ namespace PITTS
       const auto n = Aop.n() / m;
 
       y.resize(rA1*r1, n, rA2*r2);
+      const int nChunks = y.nChunks();
 
       const auto index = [n](int k, int l)
       {
@@ -53,21 +54,23 @@ namespace PITTS
          {(rA1*m*n*rA2+r1*m*r2)*kernel_info::Load<Chunk<T>>() + (rA1*r1*n*rA2*r2)*kernel_info::Store<Chunk<T>>()}} // data transfers
         );
 
-      for(int i1 = 0; i1 < rA1; i1++)
-        for(int j1 = 0; j1 < r1; j1++)
-        {
-          for(int i2 = 0; i2 < rA2; i2++)
-            for(int j2 = 0; j2 < r2; j2++)
-            {
-              for(int k = 0; k < n; k++)
-              {
-                T tmp = T(0);
-                for(int l = 0; l < m; l++)
-                  tmp += Aop(i1,index(k,l),i2) * x(j1,l,j2);
-                y(i1*r1+j1,k,i2*r2+j2) = tmp;
-              }
-            }
-        }
+
+      // resulting y is the biggest array and this is easily memory-bound, so order loops according to memory layout of y
+#pragma omp parallel for collapse(3) schedule(static)
+      for(int j = 0; j < rA2*r2; j++)
+        for(int kChunk = 0; kChunk < nChunks; kChunk++)
+          for(int i = 0; i < rA1*r1; i++)
+          {
+            const int i1 = i / r1;
+            const int j1 = i % r1;
+            const int i2 = j / r2;
+            const int j2 = j % r2;
+            Chunk<T> tmp{};
+            for(int l = 0; l < m; l++)
+              for(int k = kChunk*Chunk<T>::size; k < std::min<int>(n, (kChunk+1)+Chunk<T>::size); k++)
+                tmp[k-kChunk*Chunk<T>::size] += Aop(i1,k+n*l,i2) * x(j1,l,j2);
+            y.chunk(i,kChunk,j) = tmp;
+          }
     }
 
     //! contract Tensor3-Operator (e.g. rank-4 tensor) and Tensor3 along some dimensions: A(0,:,*,*) * x(*,:,:)
