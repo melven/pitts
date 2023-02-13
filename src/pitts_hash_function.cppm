661f16b79d917d003f3a11218b386da18e47f0bb
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2023-02-13 22:23:09 +0100

    cxx_modules: pitts_hash_function

---
+++ src/pitts_hash_function.cppm
@@ -0,0 +1,9 @@
+/*! @file pitts_multivector_cdist.cppm
+* @brief Wrapper file for generating a c++ module
+* @author Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
+* @date 2023-02-13
+* @copyright Deutsches Zentrum fuer Luft- und Raumfahrt e. V. (DLR), German Aerospace Center
+*
+**/
+#define EXPORT_PITTS_HASH_FUNCTION
+#include "pitts_hash_function.hpp"
--- src/pitts_hash_function.hpp
+++ src/pitts_hash_function.hpp
@@ -6,17 +6,33 @@
 *
 **/
 
+// just import the module if we are in module mode and this file is not included from pitts_multivector_cdist.cppm
+#if defined(PITTS_USE_MODULES) && !defined(EXPORT_PITTS_HASH_FUNCTION)
+import pitts_hash_function;
+#define PITTS_HASH_FUNCTION_HPP
+#endif
+
 // include guard
 #ifndef PITTS_HASH_FUNCTION_HPP
 #define PITTS_HASH_FUNCTION_HPP
+
+// global module fragment
+#ifdef PITTS_USE_MODULES
+module;
+#endif
 
 // includes
 #include <string_view>
 #include <cstdint>
 
+// module export
+#ifdef PITTS_USE_MODULES
+export module pitts_hash_function;
+# define PITTS_MODULE_EXPORT export
+#endif
 
 //! namespace for the library PITTS (parallel iterative tensor train solvers)
-namespace PITTS
+PITTS_MODULE_EXPORT namespace PITTS
 {
   //! namespace for helper functionality
   namespace internal
