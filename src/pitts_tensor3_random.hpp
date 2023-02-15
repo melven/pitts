5b573dd03b3683efaddb111d33f6b3b0915dbb1a
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2023-02-15 17:54:15 +0100

    cxx_modules: tensor3_random

--- src/pitts_tensor3_random.hpp
+++ src/pitts_tensor3_random.hpp
@@ -6,17 +6,40 @@
 *
 **/
 
+// just import the module if we are in module mode and this file is not included from pitts_tensor3_random.cppm
+#if defined(PITTS_USE_MODULES) && !defined(EXPORT_PITTS_TENSOR3_RANDOM)
+import pitts_tensor3_random;
+#define PITTS_TENSOR3_RANDOM_HPP
+#endif
+
 // include guard
 #ifndef PITTS_TENSOR3_RANDOM_HPP
 #define PITTS_TENSOR3_RANDOM_HPP
 
+// global module fragment
+#ifdef PITTS_USE_MODULES
+module;
+#endif
+
 // includes
+#ifdef PITTS_USE_MODULES
+// workaround for mismatching std::align implementation
+#include <memory>
+#endif
+
 #include <random>
 #include "pitts_tensor3.hpp"
 #include "pitts_timer.hpp"
 
+// module export
+#ifdef PITTS_USE_MODULES
+export module pitts_tensor3_random;
+# define PITTS_MODULE_EXPORT export
+#endif
+
+
 //! namespace for the library PITTS (parallel iterative tensor train solvers)
-namespace PITTS
+PITTS_MODULE_EXPORT namespace PITTS
 {
   //! fill a rank-3 tensor with random values
   //!
@@ -40,6 +63,9 @@ namespace PITTS
           t3(i,j,k) = distribution(randomGenerator);
   }
 
+  // explicit template instantiations
+  template void randomize<float>(Tensor3<float>& X);
+  template void randomize<double>(Tensor3<double>& X);
 }
 
 
