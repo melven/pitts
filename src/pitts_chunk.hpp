6307364f54768c0316dabfe53b07db1e4faa0ae0
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2023-02-15 21:42:40 +0100

    cxx_modules: chunk

--- src/pitts_chunk.hpp
+++ src/pitts_chunk.hpp
@@ -6,15 +6,32 @@
 *
 **/
 
+// just import the module if we are in module mode and this file is not included from pitts_multivector_cdist.cppm
+#if defined(PITTS_USE_MODULES) && !defined(EXPORT_PITTS_CHUNK)
+import pitts_chunk;
+#define PITTS_CHUNK_HPP
+#endif
+
 // include guard
 #ifndef PITTS_CHUNK_HPP
 #define PITTS_CHUNK_HPP
 
+// global module fragment
+#ifdef PITTS_USE_MODULES
+module;
+#endif
+
 // includes
 #include <array>
 
+// module export
+#ifdef PITTS_USE_MODULES
+export module pitts_chunk;
+# define PITTS_MODULE_EXPORT export
+#endif
+
 //! namespace for the library PITTS (parallel iterative tensor train solvers)
-namespace PITTS
+PITTS_MODULE_EXPORT namespace PITTS
 {
   //! global alignment (in bytes) to allow SIMD / improve memory accesses
   constexpr auto ALIGNMENT = 128;
@@ -29,6 +46,11 @@ namespace PITTS
   {
     static constexpr auto size = ALIGNMENT/sizeof(T);
   };
+
+
+  // explicit template instantiations
+  template struct Chunk<float>;
+  template struct Chunk<double>;
 }
 
 
