353ed49d1d627b4a872e98f6e7c9bb0dd97b3594
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2023-02-15 23:11:25 +0100

    cxx_modules: scope_info

---
+++ src/pitts_scope_info.cppm
@@ -0,0 +1,9 @@
+/*! @file pitts_scope_info.cppm
+* @brief Wrapper file for generating a c++ module
+* @author Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
+* @date 2023-02-13
+* @copyright Deutsches Zentrum fuer Luft- und Raumfahrt e. V. (DLR), German Aerospace Center
+*
+**/
+#define EXPORT_PITTS_SCOPE_INFO
+#include "pitts_scope_info.hpp"
--- src/pitts_scope_info.hpp
+++ src/pitts_scope_info.hpp
@@ -6,22 +6,42 @@
 *
 **/
 
+// just import the module if we are in module mode and this file is not included from pitts_scope_info.cppm
+#if defined(PITTS_USE_MODULES) && !defined(EXPORT_PITTS_SCOPE_INFO)
+import pitts_scope_info;
+#define PITTS_SCOPE_INFO_HPP
+#endif
+
 // include guard
 #ifndef PITTS_SCOPE_INFO_HPP
 #define PITTS_SCOPE_INFO_HPP
 
+// global module fragment
+#ifdef PITTS_USE_MODULES
+module;
+#endif
+
 // includes
-#include "pitts_hash_function.hpp"
-#include "pitts_type_name.hpp"
 #include <source_location>
 #include <array>
 #include <string_view>
+#include <string>
 #include <cstdint>
 #include <bit>
+#include "pitts_hash_function.hpp"
+#include "pitts_type_name.hpp"
+
+// module export
+#ifdef PITTS_USE_MODULES
+export module pitts_scope_info;
+# define PITTS_MODULE_EXPORT export
+#else
+# define PITTS_MODULE_EXPORT
+#endif
 
 
 //! namespace for the library PITTS (parallel iterative tensor train solvers)
-namespace PITTS
+PITTS_MODULE_EXPORT namespace PITTS
 {
   //! namespace for helper functionality
   namespace internal
