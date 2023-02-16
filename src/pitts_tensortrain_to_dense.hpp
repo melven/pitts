37927e49124b2ac9e292839efe82789382437b73
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2023-02-16 15:51:11 +0100

    cxx_modules: tensortrain_to_dense

--- src/pitts_tensortrain_to_dense.hpp
+++ src/pitts_tensortrain_to_dense.hpp
@@ -6,9 +6,20 @@
 *
 **/
 
+// just import the module if we are in module mode and this file is not included from pitts_tensortrain_to_dense.cppm
+#if defined(PITTS_USE_MODULES) && !defined(EXPORT_PITTS_TENSORTRAIN_TO_DENSE)
+import pitts_tensortrain_to_dense;
+#define PITTS_TENSORTRAIN_TO_DENSE_HPP
+#endif
+
 // include guard
 #ifndef PITTS_TENSORTRAIN_TO_DENSE_HPP
 #define PITTS_TENSORTRAIN_TO_DENSE_HPP
+
+// global module fragment
+#ifdef PITTS_USE_MODULES
+module;
+#endif
 
 // includes
 #include <cstddef>
@@ -24,8 +35,15 @@
 #include "pitts_multivector.hpp"
 #include "pitts_multivector_transform.hpp"
 
+// module export
+#ifdef PITTS_USE_MODULES
+export module pitts_tensortrain_to_dense;
+# define PITTS_MODULE_EXPORT export
+#endif
+
+
 //! namespace for the library PITTS (parallel iterative tensor train solvers)
-namespace PITTS
+PITTS_MODULE_EXPORT namespace PITTS
 {
   //! calculate fully dense tensor from a tensor-train decomposition (stored as a MultiVector)
   //!
@@ -132,6 +150,7 @@ namespace PITTS
       first[i] = X(i,0);
   }
 
+  // explicit template instantiations
 }
 
 
