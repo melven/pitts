1c71c2158cfacb92d30c65162b662d7caebfd857
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2023-02-16 13:12:49 +0100

    cxx_modules: tensortrain_debug

--- src/pitts_tensortrain_debug.hpp
+++ src/pitts_tensortrain_debug.hpp
@@ -8,18 +8,37 @@
 * As this generates temporaries and uses sub-optimal kernels, this is only intended for e.g. assert-statements.
 **/
 
+// just import the module if we are in module mode and this file is not included from pitts_tensortrain_debug.cppm
+#if defined(PITTS_USE_MODULES) && !defined(EXPORT_PITTS_TENSORTRAIN_DEBUG)
+import pitts_tensortrain_debug;
+#define PITTS_TENSORTRAIN_DEBUG_HPP
+#endif
+
 // include guard
 #ifndef PITTS_TENSORTRAIN_DEBUG_HPP
 #define PITTS_TENSORTRAIN_DEBUG_HPP
+
+// global module fragment
+#ifdef PITTS_USE_MODULES
+module;
+#endif
 
 // includes
 #include "pitts_tensortrain.hpp"
 #include "pitts_tensortrain_norm.hpp"
 #include "pitts_tensortrain_dot.hpp"
 #include "pitts_tensortrain_axpby.hpp"
+#include "pitts_tensortrain_normalize.hpp"
+
+// module export
+#ifdef PITTS_USE_MODULES
+export module pitts_tensortrain_debug;
+# define PITTS_MODULE_EXPORT export
+#endif
+
 
 //! namespace for the library PITTS (parallel iterative tensor train solvers)
-namespace PITTS
+PITTS_MODULE_EXPORT namespace PITTS
 {
   //! namespace for debugging functionality
   namespace debug
@@ -70,6 +89,8 @@ namespace PITTS
     [[nodiscard]] TensorTrain<T> operator/(const TensorTrain<T>& ttX, T alpha) {return T(1)/alpha * ttX;}
 
   }
+
+  // explicit template instantiations
 }
 
 #endif // PITTS_TENSORTRAIN_DEBUG_HPP
