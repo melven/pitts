fd11d310ca03da5f8984377e1102bdfece49e20a
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2023-02-16 14:32:21 +0100

    cxx_modules: tensortrain_operator_apply_op

--- src/pitts_tensortrain_operator_apply_op.hpp
+++ src/pitts_tensortrain_operator_apply_op.hpp
@@ -6,18 +6,42 @@
 *
 **/
 
+// just import the module if we are in module mode and this file is not included from pitts_tensortrain_operator_apply_op.cppm
+#if defined(PITTS_USE_MODULES) && !defined(EXPORT_PITTS_TENSORTRAIN_OPERATOR_APPLY_OP)
+import pitts_tensortrain_operator_apply_op;
+#define PITTS_TENSORTRAIN_OPERATOR_APPLY_OP_HPP
+#endif
+
 // include guard
 #ifndef PITTS_TENSORTRAIN_OPERATOR_APPLY_OP_HPP
 #define PITTS_TENSORTRAIN_OPERATOR_APPLY_OP_HPP
 
+// global module fragment
+#ifdef PITTS_USE_MODULES
+module;
+#endif
+
 // includes
 #include <cmath>
+#include <cassert>
+#include <stdexcept>
+#include <vector>
+#include "pitts_tensortrain.hpp"
 #include "pitts_tensortrain_operator.hpp"
 #include "pitts_timer.hpp"
 #include "pitts_chunk_ops.hpp"
+#include "pitts_performance.hpp"
+#include "pitts_tensor3.hpp"
+
+// module export
+#ifdef PITTS_USE_MODULES
+export module pitts_tensortrain_operator_apply_op;
+# define PITTS_MODULE_EXPORT export
+#endif
+
 
 //! namespace for the library PITTS (parallel iterative tensor train solvers)
-namespace PITTS
+PITTS_MODULE_EXPORT namespace PITTS
 {
   //! namespace for helper functionality
   namespace internal
@@ -126,6 +150,9 @@ namespace PITTS
     TTOpC.tensorTrain().setSubTensors(0, std::move(subTOpC));
   }
 
+  // explicit template instantiations
+  template void apply<float>(const TensorTrainOperator<float>& TTOp, const TensorTrainOperator<float>& TTx, TensorTrainOperator<float>& TTy);
+  template void apply<double>(const TensorTrainOperator<double>& TTOp, const TensorTrainOperator<double>& TTx, TensorTrainOperator<double>& TTy);
 }
 
 
