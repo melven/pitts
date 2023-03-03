5edc547391a0436289af2deba6042b3340c6a582
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2023-03-03 23:19:13 +0100

    impl_files: tensortrain_to_dense

---
+++ src/pitts_tensortrain_to_dense.cpp
@@ -0,0 +1,24 @@
+// only used for PITTS_DEVELOPMENT_BUILD
+#ifndef PITTS_DEVELOP_BUILD
+#error "pitts is a header-only library, .cpp files should only used internally to speedup pitts compile times"
+#endif
+
+// actually generate code for corresponding _impl.hpp file
+#include "pitts_tensortrain_to_dense_impl.hpp"
+
+using namespace PITTS;
+
+template void PITTS::toDense<double>(const TensorTrain<double>& TT, MultiVector<double>& X);
+template void PITTS::toDense<float>(const TensorTrain<float>& TT, MultiVector<float>& X);
+
+template<typename T>
+using Iter = T*;
+
+template void PITTS::toDense<double, Iter<double>>(const TensorTrain<double>& TT, Iter<double>, Iter<double>);
+template void PITTS::toDense<float, Iter<float>>(const TensorTrain<float>& TT, Iter<float>, Iter<float>);
+
+template<typename T>
+using StdIter = decltype(std::vector<T>().begin());
+
+template void PITTS::toDense<double, StdIter<double>>(const TensorTrain<double>& TT, StdIter<double>, StdIter<double>);
+template void PITTS::toDense<float, StdIter<float>>(const TensorTrain<float>& TT, StdIter<float>, StdIter<float>);
