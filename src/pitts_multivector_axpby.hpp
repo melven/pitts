5c08dc479ca36bcdbe09afb54681d80115828904
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2023-03-03 13:27:40 +0100

    impl_files: multivector_axpby

--- src/pitts_multivector_axpby.hpp
+++ src/pitts_multivector_axpby.hpp
@@ -12,11 +12,8 @@
 
 // includes
 #include <array>
-#include <stdexcept>
 #include "pitts_eigen.hpp"
 #include "pitts_multivector.hpp"
-#include "pitts_performance.hpp"
-#include "pitts_chunk_ops.hpp"
 
 //! namespace for the library PITTS (parallel iterative tensor train solvers)
 namespace PITTS
@@ -32,35 +29,8 @@ namespace PITTS
   //! @param Y        resulting multi-vector, dimensions (n, m)
   //!
   template<typename T>
-  void axpy(const Eigen::ArrayX<T>& alpha, const MultiVector<T>& X, MultiVector<T>& Y)
-  {
-    // check dimensions
-    if( X.cols() != Y.cols() || alpha.size() != X.cols() )
-      throw std::invalid_argument("MultiVector::axpy: cols dimension mismatch!");
-
-    if( X.rows() != Y.rows() )
-      throw std::invalid_argument("MultiVector::axpy: rows dimension mismatch!");
-
-    const auto nChunks = X.rowChunks();
-    const auto nCols = Y.cols();
-
-    // gather performance data
-    const auto timer = PITTS::performance::createScopedTimer<MultiVector<T>>(
-        {{"nChunks", "nCols"},{nChunks, nCols}}, // arguments
-        {{nChunks*nCols*Chunk<T>::size*kernel_info::FMA<T>()}, // flops
-         {nChunks*nCols*kernel_info::Load<Chunk<T>>() + nChunks*nCols*kernel_info::Update<T>()}} // data transfers
-        );
-
-#pragma omp parallel
-    {
-      for(int iCol = 0; iCol < nCols; iCol++)
-      {
-#pragma omp for schedule(static) nowait
-        for(int iChunk = 0; iChunk < nChunks; iChunk++)
-          fmadd(alpha(iCol), X.chunk(iChunk,iCol), Y.chunk(iChunk,iCol));
-      }
-    }
-  }
+  void axpy(const Eigen::ArrayX<T>& alpha, const MultiVector<T>& X, MultiVector<T>& Y);
+  
 
   //! calculate the pair-wise scaled addition of the columns of two multi-vectors, and the norm of the result
   //!
@@ -75,47 +45,8 @@ namespace PITTS
   //! @returns        array of norms of the resulting y, dimension (m)
   //!
   template<typename T>
-  auto axpy_norm2(const Eigen::ArrayX<T>& alpha, const MultiVector<T>& X, MultiVector<T>& Y)
-  {
-    // check dimensions
-    if( X.cols() != Y.cols() || alpha.size() != X.cols() )
-      throw std::invalid_argument("MultiVector::axpy_norm2: cols dimension mismatch!");
-
-    if( X.rows() != Y.rows() )
-      throw std::invalid_argument("MultiVector::axpy_norm2: rows dimension mismatch!");
-
-    const auto nChunks = X.rowChunks();
-    const auto nCols = Y.cols();
-
-    // gather performance data
-    const auto timer = PITTS::performance::createScopedTimer<MultiVector<T>>(
-        {{"nChunks", "nCols"},{nChunks, nCols}}, // arguments
-        {{2*nChunks*nCols*Chunk<T>::size*kernel_info::FMA<T>()}, // flops
-         {nChunks*nCols*kernel_info::Load<Chunk<T>>() + nChunks*nCols*kernel_info::Update<T>() + nCols*kernel_info::Store<T>()}} // data transfers
-        );
-
-    T tmp[nCols];
-    for(int iCol = 0; iCol < nCols; iCol++)
-      tmp[iCol] = T(0);
-#pragma omp parallel reduction(+:tmp)
-    {
-      for(int iCol = 0; iCol < nCols; iCol++)
-      {
-        Chunk<T> tmpChunk{};
-#pragma omp for schedule(static) nowait
-        for(int iChunk = 0; iChunk < nChunks; iChunk++)
-        {
-          fmadd(alpha(iCol), X.chunk(iChunk,iCol), Y.chunk(iChunk,iCol));
-          fmadd(Y.chunk(iChunk,iCol), Y.chunk(iChunk,iCol), tmpChunk);
-        }
-        tmp[iCol] = sum(tmpChunk);
-      }
-    }
-
-    using arr = Eigen::ArrayX<T>;
-    arr result = Eigen::Map<arr>(tmp, nCols).sqrt();
-    return result;
-  }
+  Eigen::ArrayX<T> axpy_norm2(const Eigen::ArrayX<T>& alpha, const MultiVector<T>& X, MultiVector<T>& Y);
+  
 
   //! calculate the pair-wise scaled addition of the columns of two multi-vectors, and the dot product of the result with the columsn of a third multi-vector
   //!
@@ -132,48 +63,12 @@ namespace PITTS
   //! @returns        array of dot products between Y and Z, dimension (m)
   //!
   template<typename T>
-  auto axpy_dot(const Eigen::ArrayX<T>& alpha, const MultiVector<T>& X, MultiVector<T>& Y, const MultiVector<T>& Z)
-  {
-    // check dimensions
-    if( X.cols() != Y.cols() || X.cols() != Z.cols() || alpha.size() != X.cols() )
-      throw std::invalid_argument("MultiVector::axpy_dot: cols dimension mismatch!");
-
-    if( X.rows() != Y.rows() || X.rows() != Z.rows() )
-      throw std::invalid_argument("MultiVector::axpy_dot: rows dimension mismatch!");
-
-    const auto nChunks = X.rowChunks();
-    const auto nCols = Y.cols();
-
-    // gather performance data
-    const auto timer = PITTS::performance::createScopedTimer<MultiVector<T>>(
-        {{"nChunks", "nCols"},{nChunks, nCols}}, // arguments
-        {{2*nChunks*nCols*Chunk<T>::size*kernel_info::FMA<T>()}, // flops
-         {nChunks*nCols*kernel_info::Load<Chunk<T>>() + nChunks*nCols*kernel_info::Update<T>() + nCols*kernel_info::Store<T>()}} // data transfers
-        );
-
-    T tmp[nCols];
-    for(int iCol = 0; iCol < nCols; iCol++)
-      tmp[iCol] = T(0);
-#pragma omp parallel reduction(+:tmp)
-    {
-      for(int iCol = 0; iCol < nCols; iCol++)
-      {
-        Chunk<T> tmpChunk{};
-#pragma omp for schedule(static) nowait
-        for(int iChunk = 0; iChunk < nChunks; iChunk++)
-        {
-          fmadd(alpha(iCol), X.chunk(iChunk,iCol), Y.chunk(iChunk,iCol));
-          fmadd(Y.chunk(iChunk,iCol), Z.chunk(iChunk,iCol), tmpChunk);
-        }
-        tmp[iCol] = sum(tmpChunk);
-      }
-    }
-
-    using arr = Eigen::ArrayX<T>;
-    arr result = Eigen::Map<arr>(tmp, nCols);
-    return result;
-  }
+  Eigen::ArrayX<T> axpy_dot(const Eigen::ArrayX<T>& alpha, const MultiVector<T>& X, MultiVector<T>& Y, const MultiVector<T>& Z);
+  
 }
 
+#ifndef PITTS_DEVELOP_BUILD
+#include "pitts_multivector_axpby_impl.hpp"
+#endif
 
 #endif // PITTS_MULTIVECTOR_AXPBY_HPP
