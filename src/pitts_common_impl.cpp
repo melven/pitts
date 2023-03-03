554f9d9b052c8f8dffc0376f999d944f3bf89617
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2023-03-03 09:43:10 +0100

    impl_files: pitts_common

---
+++ src/pitts_common_impl.cpp
@@ -0,0 +1,10 @@
+// only used for PITTS_DEVELOPMENT_BUILD
+#ifndef PITTS_DEVELOP_BUILD
+#error "pitts is a header-only library, .cpp files should only used internally to speedup pitts compile times"
+#endif
+// actually generate code for corresponding _impl.hpp file
+#include "pitts_common_impl.hpp"
+
+
+void initialize(int* argc, char** argv[], bool verbose);
+void finalize(bool verbose);
