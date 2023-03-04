2c28c3635f1828783c46d095df53623068ef285a
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2023-03-04 09:00:03 +0100

    impl_files: bug fix for release + compile bench script

---
+++ bench/compile/build_clang_devbuild.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+# abort on errors
+set -xe
+
+time source ~/load_modules_pitts_modules.sh > /dev/null
+
+BUILD_DIR=$(mktemp -d -p . build.XXXX) && cd $BUILD_DIR
+
+time cmake -Wno-dev -G Ninja -DCMAKE_BUILD_TYPE=$1 -DPITTS_EIGEN_USE_LAPACKE=On -DPITTS_USE_MODULES=Off -DCMAKE_CXX_COMPILER=clang++ -DPITTS_DEVELOP_BUILD=On ../../.. > cmake.log
+
+time ninja > ninja1.log
+
+time ninja pitts_tests > ninja2.log
+
+touch ../../../src/pitts_tensortrain_dot_impl.hpp
+time ninja > ninja3.log
+
+touch ../../../src/pitts_tensortrain_dot_impl.hpp
+time ninja pitts_tests > ninja4.log
--- src/pitts_tensortrain_axpby_normalized.cpp
+++ src/pitts_tensortrain_axpby_normalized.cpp
@@ -10,3 +10,6 @@ using namespace PITTS;
 
 template double PITTS::internal::axpby_normalized<double>(double alpha, const TensorTrain<double>& TTx, double beta, TensorTrain<double>& TTy, double rankTolerance, int maxRank);
 template float PITTS::internal::axpby_normalized<float>(float alpha, const TensorTrain<float>& TTx, float beta, TensorTrain<float>& TTy, float rankTolerance, int maxRank);
+
+template bool PITTS::internal::is_normalized<double>(const TensorTrain<double>& A, TT_Orthogonality orthog, double eps);
+template bool PITTS::internal::is_normalized<float>(const TensorTrain<float>& A, TT_Orthogonality orthog, double eps);
