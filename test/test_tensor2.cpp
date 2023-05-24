5da82e21a5fbb02fe37cf8890005dd11a98289fb
Becklas, Manuel Joey <manuel.becklas@dlr.de>
2023-05-24 17:42:16 +0000

    Implement tensor2 view

--- test/test_tensor2.cpp
+++ test/test_tensor2.cpp
@@ -26,26 +26,6 @@ TEST(PITTS_Tensor2, create)
   ASSERT_EQ(7, M.r2());
 }
 
-// anonymous namespace
-namespace
-{
-// helper class for accessing the protected member chunkSize
-template<class T2>
-struct ChunkSize : private T2
-{
-  static constexpr auto value = T2::chunkSize;
-};
-}
-
-TEST(PITTS_Tensor2, chunkSize)
-{
-  using Chunk_double = PITTS::Chunk<double>;
-  using Tensor2_double = PITTS::Tensor2<double>;
-  Chunk_double chunk;
-
-  EXPECT_EQ(chunk.size, ChunkSize<Tensor2_double>::value);
-}
-
 TEST(PITTS_Tensor2, resize)
 {
   using Tensor2_double = PITTS::Tensor2<double>;
@@ -127,4 +107,52 @@ TEST(PITTS_Tensor2, operator_indexing)
     {
       EXPECT_EQ(i*7+j, M(i,j));
     }
+}
+
+/**********************/
+/* (Const)Tensor2View */
+/**********************/
+
+using namespace PITTS;
+
+TEST(PITTS_Tensor2, View)
+{
+    const int r1 = 3, r2 = 7;
+
+    Tensor2<double> t2(r1,r2);
+    for (int j = 0; j < r2; j++)
+      for (int i = 0; i < r1; i++)
+        t2(i,j) = i + j*r1; // unique value for each entry
+    
+    Tensor2View<double> t2view((Chunk<double>*)&t2(0,0), t2.r1(), t2.r2());
+
+    // dimensions must match
+    ASSERT_EQ(r1, t2view.r1());
+    ASSERT_EQ(r2, t2view.r2());
+    // values must match
+    for (int j = 0; j < r2; j++)
+      for (int i = 0; i < r1; i++)
+        EXPECT_EQ(t2(i,j), t2view(i,j));
+}
+
+TEST(PITTS_Tensor2, ConstView)
+{
+    const int r1 = 3, r2 = 7;
+
+    Tensor2<double> t2(r1,r2);
+    for (int j = 0; j < r2; j++)
+      for (int i = 0; i < r1; i++)
+        t2(i,j) = i + j*r1; // unique value for each entry
+    
+    Tensor2View<double> t2view((Chunk<double>*)&t2(0,0), t2.r1(), t2.r2());
+
+    // dimensions must match
+    ASSERT_EQ(r1, t2view.r1());
+    ASSERT_EQ(r2, t2view.r2());
+    // values must match and changes must propogate
+    t2(1,2) = -12;
+    t2view(2,1) = -21;
+    for (int j = 0; j < r2; j++)
+      for (int i = 0; i < r1; i++)
+        EXPECT_EQ(t2(i,j), t2view(i,j));
 }
--- test/test_tensor3_unfold.cpp
+++ test/test_tensor3_unfold.cpp
@@ -4,6 +4,8 @@
 #include "pitts_tensor3_random.hpp"
 #include "pitts_eigen.hpp"
 
+using namespace PITTS;
+
 
 TEST(PITTS_Tensor3_unfold, unfold_left_Eigen_scalar)
 {
@@ -215,4 +217,110 @@ TEST(PITTS_Tensor3_unfold, unfold_move_multivector)
       {
         EXPECT_NEAR(t3_ref(i,j,k), mv(i+j*3+k*5*3,0), eps);
       }
+}
+
+TEST(PITTS_Tensor3_unfold, constView_left)
+{
+    const int r1 = 3, n = 5, r2 = 7;
+
+    Tensor3<double> t3(r1,n,r2);
+    for (int k = 0; k < r2; k++)
+        for (int j = 0; j < n; j++)
+            for (int i = 0; i < r1; i++)
+                t3(i,j,k) = i + j*r1 + j*r1*n; // unique value for each entry
+    const Tensor3<double>& t3Const = t3;
+    
+    ConstTensor2View<double> t2 = unfold_left(t3Const);
+
+    // dimensions must match
+    ASSERT_EQ(r1*n, t2.r1());
+    ASSERT_EQ(r2,   t2.r2());
+    //ASSERT_EQ(t3.reservedChunks(), t2.reservedChunks());
+    // values must match
+    for (int k = 0; k < r2; k++)
+        for (int j = 0; j < n; j++)
+            for (int i = 0; i < r1; i++)
+                EXPECT_EQ(t3(i,j,k), t2(i+j*r1,k));
+}
+
+TEST(PITTS_Tensor3_unfold, view_left)
+{
+    const int r1 = 3, n = 5, r2 = 7;
+
+    Tensor3<double> t3(r1,n,r2);
+    for (int k = 0; k < r2; k++)
+        for (int j = 0; j < n; j++)
+            for (int i = 0; i < r1; i++)
+                t3(i,j,k) = i + j*r1 + j*r1*n; // unique value for each entry
+    
+     Tensor2View<double> t2 = unfold_left(t3);
+
+    // dimensions must match
+    ASSERT_EQ(r1*n, t2.r1());
+    ASSERT_EQ(r2,   t2.r2());
+    //ASSERT_EQ(t3.reservedChunks(), t2.reservedChunks());
+    // values must match
+    for (int k = 0; k < r2; k++)
+        for (int j = 0; j < n; j++)
+            for (int i = 0; i < r1; i++)
+                EXPECT_EQ(t3(i,j,k), t2(i+j*r1,k));
+    // changes must propogate
+    t3(1,2,3) = -123;
+    EXPECT_EQ(-123, t2(1+2*r1,3));
+    t2(2+1*r1,0) = -210;
+    EXPECT_EQ(-210, t3(2,1,0));
+}
+
+TEST(PITTS_Tensor3_unfold, constView_right)
+{
+    const int r1 = 3, n = 5, r2 = 7;
+
+    Tensor3<double> t3(r1,n,r2);
+    for (int k = 0; k < r2; k++)
+        for (int j = 0; j < n; j++)
+            for (int i = 0; i < r1; i++)
+                t3(i,j,k) = i + j*r1 + j*r1*n; // unique value for each entry
+    const Tensor3<double>& t3Const = t3;
+
+    
+    ConstTensor2View<double> t2 = unfold_right(t3Const);
+
+    // dimensions must match
+    ASSERT_EQ(r1,   t2.r1());
+    ASSERT_EQ(n*r2, t2.r2());
+    //ASSERT_EQ(t3.reservedChunks(), t2.reservedChunks());
+    // values must match
+    for (int k = 0; k < r2; k++)
+        for (int j = 0; j < n; j++)
+            for (int i = 0; i < r1; i++)
+                EXPECT_EQ(t3(i,j,k), t2(i,j+k*n));
+}
+
+
+TEST(PITTS_Tensor3_unfold, view_right)
+{
+    const int r1 = 3, n = 5, r2 = 7;
+    
+    Tensor3<double> t3(r1,n,r2);
+    for (int k = 0; k < r2; k++)
+        for (int j = 0; j < n; j++)
+            for (int i = 0; i < r1; i++)
+                t3(i,j,k) = i + j*r1 + j*r1*n; // unique value for each entry
+    
+    Tensor2View<double> t2 = unfold_right(t3);
+
+    // dimensions must match
+    ASSERT_EQ(r1,   t2.r1());
+    ASSERT_EQ(n*r2, t2.r2());
+    //ASSERT_EQ(t3.reservedChunks(), t2.reservedChunks());
+    // values must match
+    for (int k = 0; k < r2; k++)
+        for (int j = 0; j < n; j++)
+            for (int i = 0; i < r1; i++)
+                EXPECT_EQ(t3(i,j,k), t2(i,j+k*n));
+    // changes must propogate
+    t3(1,2,3) = -123;
+    EXPECT_EQ(-123, t2(1,2+3*n));
+    t2(2,1+0*n) = -210;
+    EXPECT_EQ(-210, t3(2,1,0));
 }
