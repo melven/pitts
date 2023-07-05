6f260290ac118e0fc2a4a453c7be387576ce4769
Manuel Joey Becklas <manuel.becklas@dlr.de>
2023-07-05 14:25:05 +0200

    rm_copy_unfold: allow non-deprecated copy unfolds for multivectors (for now)

--- src/pitts_tensor3_unfold.hpp
+++ src/pitts_tensor3_unfold.hpp
@@ -97,6 +97,68 @@ namespace PITTS
         }
   }
 
+  //! reshape a 3d tensor to a matrix combining the first two dimensions (r1*n x r2)
+  //!
+  //! @tparam T             underlying data type (double, complex, ...)
+  //! @tparam MatrixType    class for the matrix, must support resize and (i,j) element-wise access
+  //!
+  //! @param t3     3d input tensor of dimension (r1,n,r2)
+  //! @param mat    2d output tensor resized to dimension (r1*n,r2)
+  //!
+  template<typename T>
+  void unfold_left(const Tensor3<T>& t3,  MultiVector<T>& mat)
+  {
+    const auto r1 = t3.r1();
+    const auto n = t3.n();
+    const auto r2 = t3.r2();
+
+    const auto timer = PITTS::performance::createScopedTimer<Tensor3<T>>(
+        {{"r1", "n", "r2"}, {r1, n, r2}},   // arguments
+        {{r1*n*r2*kernel_info::NoOp<T>()},    // flops
+         {r1*n*r2*kernel_info::Store<T>() + r1*n*r2*kernel_info::Load<T>()}}  // data
+        );
+
+    mat.resize(r1*n, r2);
+#pragma omp parallel for collapse(3) schedule(static) if(r1*n*r2 > 500)
+    for (int k = 0; k < r2; k++)
+      for (int j = 0; j < n; j++)
+        for (int i = 0; i < r1; i++)
+        {
+          mat(i+j*r1,k) = t3(i,j,k);
+        }
+  }
+
+  //! reshape a 3d tensor to a matrix combining the last two dimensions (r1 x n*r2)
+  //!
+  //! @tparam T             underlying data type (double, complex, ...)
+  //! @tparam MatrixType    class for the matrix, must support resize and (i,j) element-wise access
+  //!
+  //! @param t3     3d input tensor of dimension (r1,n,r2)
+  //! @param mat    2d output tensor resized to dimension (r1,n*r2)
+  //!
+  template<typename T>
+  void unfold_right(const Tensor3<T>& t3, MultiVector<T>& mat)
+  {
+    const auto r1 = t3.r1();
+    const auto n = t3.n();
+    const auto r2 = t3.r2();
+
+    const auto timer = PITTS::performance::createScopedTimer<Tensor3<T>>(
+        {{"r1", "n", "r2"}, {r1, n, r2}},   // arguments
+        {{r1*n*r2*kernel_info::NoOp<T>()},    // flops
+         {r1*n*r2*kernel_info::Store<T>() + r1*n*r2*kernel_info::Load<T>()}}  // data
+        );
+
+    mat.resize(r1, n*r2);
+#pragma omp parallel for collapse(3) schedule(static) if(r1*n*r2 > 500)
+    for (int k = 0; k < r2; k++)
+      for (int j = 0; j < n; j++)
+        for (int i = 0; i < r1; i++)
+        {
+          mat(i,j+k*n) = t3(i,j,k);
+        }
+  }
+
   //! reshape a 3d tensor to a vector flattening dimensions
   //!
   //! @tparam T             underlying data type (double, complex, ...)
