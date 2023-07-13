680afcd566f302555003fc02ea94f0abe70789d2
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2023-07-13 09:42:15 +0200

    bench: updated analyze script and support QTT in mals script

--- bench/sc-cluster/tt_mals/perfctr_analyze.sh
+++ bench/sc-cluster/tt_mals/perfctr_analyze.sh
@@ -5,11 +5,13 @@ if [ "$1" == "" ]; then
   exit
 fi
 
-rm -f t1 t2 t3 t4_ t4 t5
-n=""
+rm -f t0_ t1 t2 t3 t4_ t4 t5
+#n=""
 
 for f in $*; do
   echo "# $f"
+
+  fgrep '# Arguments' $f | sed 's/.*(n=//' | sed 's/, d=/ /' | sed "s/, I=.*rhs_type='/ /" | sed "s/', rhs_random_rank=/ /" | sed 's/, lhs_type.*//' | sed 's/rhs_ones.*/ones/' | sed 's/rhs_random //' >> t0_
 
   fgrep RDTSC $f | grep -v '|.*|.*|.*|.*|.*|.*|' | sed 's/STAT *|[ 0-9.]*|[ 0-9.]*|[ 0-9.]*//' | awk '{print $6}' >> t1
   fgrep "  DP [MFLOP" $f | grep -v '|.*|.*|.*|.*|.*|.*|' | sed 's/STAT//' | awk '{print $5}' >> t2
@@ -25,22 +27,22 @@ for f in $*; do
 
   fgrep 'GMRES(' $f | sed 's/.*double.*] *//' | awk '{print $2}' >> t5
 
-  n="$n $(cat t1 | wc -l)"
+  #n="$n $(cat t1 | wc -l)"
 done
 
 paste t4_ t5 | awk '{print $1-$2}' > t4
 paste t1 t2 | awk '{print $1*$2}' > t6
 
-n0=$(echo "$n" | awk '{print $1}')
-n1=0
-if [ "$2" != "" ]; then
-  n1=$(echo "$n" | awk '{print $2-$1}')
-fi
-#echo "n: $n"
-#echo "n0: $n0, n1: $n1"
-
-head -n $n0 t0 > t0_
-tail -n $n1 t0 >> t0_
+# n0=$(echo "$n" | awk '{print $1}')
+# n1=0
+# if [ "$2" != "" ]; then
+#   n1=$(echo "$n" | awk '{print $2-$1}')
+# fi
+# #echo "n: $n"
+# #echo "n0: $n0, n1: $n1"
+# 
+# head -n $n0 t0 > t0_
+# tail -n $n1 t0 >> t0_
 
 echo '# n d r_rhs      runtime   MFLOP/s   runtime_unhalted  "total GMRES iter"  "GMRES calls" "Total MFlops"'
 paste t0_ t1 t2 t3 t4 t5 t6
--- bench/sc-cluster/tt_mals/tt_mals.py
+++ bench/sc-cluster/tt_mals/tt_mals.py
@@ -29,6 +29,7 @@ if __name__ == '__main__':
     parser.add_argument('-Rr', type=int, help='TT-ranks of the random part of the operator', default=2)
     parser.add_argument('-L', type=float, help='Coefficient of the Laplace/diffusion part of the operator', default=1.0)
     parser.add_argument('-C', type=float, help='Coefficient of the convection part of the operator, gets scaled by 1/sqrt(d) to obtain a dimension-independent constant convection velocity', default=0.1)
+    parser.add_argument('--SpinChainSZ', type=float, help='Coefficient of the SpinChain part of the operator', default=0)
 
     # preconditioner setup
     parser.add_argument('--preconditioner', type=str, help='type of the preconditioner', choices=['none', 'TT-rank1'], default='none')
@@ -52,6 +53,7 @@ if __name__ == '__main__':
     parser.add_argument('--maxRank', type=int, default=150, help='max. TT rank')
     parser.add_argument('--gmresMaxIter', type=int, help='max. number of inner GMRES iterations', default=100)
     parser.add_argument('--gmresRelTol', type=float, help='relative tolerance for the inner GMRES iterations', default=1.e-3)
+    parser.add_argument('--toQttOp', action='store_true', help='Convert operator to quantized tensor-train format.')
 
 
     args = parser.parse_args()
@@ -99,6 +101,21 @@ if __name__ == '__main__':
         symmetric = False
         kappa_est += args.C * 1.3 # * n / n
         kappa_w += args.C/args.n
+    if args.SpinChainSZ != 0.0:
+        # build spin-chain operator with ITensor
+        assert(args.n == 2)
+        siteSet = pitts_py.itensor.SpinHalf(args.d)
+        
+        ampo = pitts_py.itensor.AutoMPO(siteSet)
+        for j in range(1, args.d):
+            ampo += 0.5,"S+",j,"S-",j+1
+            ampo += 0.5,"S-",j,"S+",j+1
+            ampo +=     "Sz",j,"Sz",j+1
+
+        TTOpSpinChain = pitts_py.itensor.toTTOp(ampo)
+        pitts_py.axpby(args.SpinChainSZ, TTOpSpinChain, 1, TTOp)
+        kappa_est += args.SpinChinSZ
+        kappa_w += args.SpinChinSZ
     print('# TTOp dims/ranks:')
     print(TTOp)
     kappa_est /= kappa_w
