251e2a7f8c35c6b8957577025d91f3dcc66b8507
Becklas <beck_mo@fe-store02.maas>
2023-09-20 16:20:50 +0200

    TSQR_parallelize: Add additional benchmark script and retroactively add results
    The new script benchmarks use cases where m is larger (500 <= m <= 1500, n=50000).

---
+++ bench/sc-cluster/tsqr_single_socket_wide.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 16
+#SBATCH -t 600
+#SBATCH --nodelist=be-cpu05
+#SBATCH --output="%x-%j.out"
+
+for ((i=500; i <= 1500; i+=100)); do
+  srun likwid-pin -c 0-15 ../../build/src/tsqr_bench 50000 $i 0 50
+done
