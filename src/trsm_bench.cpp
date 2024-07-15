899760b720f6165410c8a4cae3ccb8c87a127bfb
Röhrig-Zöllner, Melven <melven.roehrig-zoellner@dlr.de>
2024-07-15 10:23:39 +0200

    trsm_bench: add missing include

--- src/trsm_bench.cpp
+++ src/trsm_bench.cpp
@@ -5,6 +5,7 @@
 
 #include "pitts_mkl.hpp"
 #include "pitts_parallel.hpp"
+#include "pitts_performance.hpp"
 #include "pitts_common.hpp"
 #include "pitts_tensor2.hpp"
 #include "pitts_tensor2_random.hpp"
